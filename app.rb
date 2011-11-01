8336d9342c8105481907fa665dfd3e1243eb2cd2
Gareth Rushgrove <gareth@alphagov.co.uk>
2011-11-01 10:15:28 +0000

    separate templates and provide placeholder class with sample data for rendering the frontend

--- app.rb
+++ app.rb
@@ -1,8 +1,43 @@
-require "sinatra"
-require "slimmer"
+require 'sinatra'
+require 'slimmer'
+require 'erubis'
 
-use Slimmer::App, :template_path => "./public/templates"
+require_relative 'helpers'
+require_relative 'config'
+
+class SearchEngine
+  def search(query)
+    return [
+      {:title => "TITLE1", :description => "DESCRIPTION", :format => "local_transaction", :link => "/URL"},
+      {:title => "TITLE1.1", :description => "DESCRIPTION", :format => "guide", :link => "/URL", :additional_links => [
+        {:title => "SUB TITLE 1", :link => "/SUBURL"},
+        {:title => "SUB TITLE 2", :link => "/SUBURL"},
+        {:title => "SUB TITLE 3", :link => "/SUBURL"}
+      ]},
+      {:title => "TITLE2", :description => "DESCRIPTION", :format => "answer", :link => "/URL"},
+      {:title => "TITLE3", :description => "DESCRIPTION", :format => "local_transaction", :link => "/URL"},
+      {:title => "TITLE4", :description => "DESCRIPTION", :format => "FORMAT", :link => "/URL"},
+      {:title => "TITLE5", :description => "DESCRIPTION", :format => "FORMAT", :link => "/URL"},
+      {:title => "TITLE6", :description => "DESCRIPTION", :format => "FORMAT", :link => "/URL"},
+      {:title => "TITLE7", :description => "DESCRIPTION", :format => "FORMAT", :link => "/URL"},
+      {:title => "TITLE8", :description => "DESCRIPTION", :format => "FORMAT", :link => "/URL"},
+      {:title => "TITLE9", :description => "DESCRIPTION", :format => "FORMAT", :link => "/URL"},
+      {:title => "TITLE10", :description => "DESCRIPTION", :format => "FORMAT", :link => "/URL"},
+      {:title => "TITLE11", :description => "DESCRIPTION", :format => "FORMAT", :link => "/URL"},
+      {:title => "TITLE12", :description => "DESCRIPTION", :format => "FORMAT", :link => "/URL"}
+    ]
+  end
+
+end
 
 get "/search" do
-  erb :search
+  @query = params['q']
+  @results = SearchEngine.new().search(@query)
+  if @query.nil?
+    erb :no_search_term
+  elsif @results.empty?
+    erb :no_search_results
+  else
+    erb :search
+  end
 end
--- test/integration/search_test.rb
+++ test/integration/search_test.rb
@@ -1,4 +1,5 @@
 require "test_helper"
+require "mocha"
 
 require File.dirname(__FILE__) + "/../../app"
 
@@ -12,5 +13,24 @@ class SearchTest < Test::Unit::TestCase
   def test_search_view_renders_successfully
     get "/search"
     assert last_response.ok?
+  end
+
+  def test_search_view_with_no_query
+    get "/search"
+    assert last_response.ok?
+    assert last_response.body.include?("You haven't specified a search query")
+  end
+
+  def test_search_view_with_query
+    get "/search", :q => 'bob'
+    assert last_response.ok?
+    assert last_response.body.include?("results for bob")
+  end
+
+  def test_search_view_returning_no_results
+    SearchEngine.any_instance.stubs(:search).returns([])
+    get "/search", :q => 'bob'
+    assert last_response.ok?
+    assert last_response.body.include?("We can&rsquo;t find any results")
   end
 end
