965c29c9537510f9843273e9a495659360f86d47
Paul Battley <pbattley@gmail.com>
2011-11-04 09:44:43 +0000

    Save documents to Solr with commit within 5 minutes.

--- lib/solr_wrapper.rb
+++ lib/solr_wrapper.rb
@@ -1,9 +1,11 @@
 class SolrWrapper
+  COMMIT_WITHIN = 5 * 60 * 1000 # 5m in ms
+
   def initialize(client)
     @client = client
   end
 
   def add(document)
-    @client.update! document.solr_export
+    @client.update! document.solr_export, commitWithin: COMMIT_WITHIN
   end
 end
--- test/unit/solr_wrapper_test.rb
+++ test/unit/solr_wrapper_test.rb
@@ -3,12 +3,22 @@ require "solr_wrapper"
 
 class SolrWrapperTest < Test::Unit::TestCase
   def test_should_update_solr_client_with_exported_document
-    document = mock("document")
-    document.stubs(:solr_export).returns("<EXPORTED/>")
+    solr_document = stub("solr document", xml: "<EXPORTED/>")
+    document = stub("document", solr_export: solr_document)
     client = mock("client")
     wrapper = SolrWrapper.new(client)
 
-    client.expects(:update!).with("<EXPORTED/>")
+    client.expects(:update!).with(solr_document, anything)
+    wrapper.add document
+  end
+
+  def test_should_tell_solr_to_commit_within_five_minutes
+    solr_document = stub("solr document", xml: "<EXPORTED/>")
+    document = stub("document", solr_export: solr_document)
+    client = mock("client")
+    wrapper = SolrWrapper.new(client)
+
+    client.expects(:update!).with(anything, has_entry(commitWithin: 5*60*1000))
     wrapper.add document
   end
 end
