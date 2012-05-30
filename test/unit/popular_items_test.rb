b291d5968343e2f20f352ecc9d2f8beec7ea75e1
Kushal Pisavadia <kushi.p@gmail.com>
2012-05-30 14:48:34 +0100

    clean up trailing whitespace

--- test/unit/popular_items_test.rb
+++ test/unit/popular_items_test.rb
@@ -9,7 +9,7 @@ class PopularItemsTest < Test::Unit::TestCase
   test "can read popular items from file" do
     assert_equal 1, @popular_items.items.count
   end
-  
+
   test "items are stored indexed by the section parameter" do
     assert_equal 2, @popular_items.items['section-name'].count
   end
@@ -19,26 +19,26 @@ class PopularItemsTest < Test::Unit::TestCase
     assert ! @popular_items.popular?('section-name', 'not-popular')
     assert ! @popular_items.popular?('other-section', 'article-slug')
   end
-  
+
   test "can select popular items from solr results by slug" do
     solr_results = [
       Document.from_hash("title" => 'Life in the UK', "link" => "/life-in-the-uk"),
       Document.from_hash("title" => 'Article Title', "link" => "/article-slug")
     ]
-    
+
     items = @popular_items.select_from('section-name', solr_results)
     assert_equal 1, items.count
     assert_equal 'Article Title', items.first.title
   end
-  
+
   test "order of popular items is controlled by the file" do
     solr_results = [
       Document.from_hash("title" => 'Two', "link" => "/article-slug-two"),
       Document.from_hash("title" => 'One', "link" => "/article-slug")
     ]
-    
+
     items = @popular_items.select_from('section-name', solr_results)
     assert_equal %w{One Two}, items.map(&:title)
   end
-  
+
 end
