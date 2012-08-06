f4ea8d6af15e01dc83379137f4522f6f239f2155
Kushal Pisavadia <kushi.p@gmail.com>
2012-08-06 16:24:17 +0000

    Add stubs for whitehall's SolrWrapper object
    Create a stub for each of the SolrWrapper objects we create. Keep these
    physically seperate so that we can still use the same API through our
    code.
    
    The DelSolr stubs were needed because you can't do fine-grained matchers
    using Mocha.

--- test/integration_test_helper.rb
+++ test/integration_test_helper.rb
@@ -57,7 +57,13 @@ class IntegrationTest < Test::Unit::TestCase
   end
 
   def setup
-    @solr = stub_everything("Solr wrapper")
-    SolrWrapper.stubs(:new).returns(@solr)
+    @solr = stub_everything("Mainstream Solr wrapper")
+    @whitehall_solr = stub_everything("Whitehall Solr wrapper")
+
+    DelSolr::Client.stubs(:new).with(settings.solr).returns(:mainstream_client)
+    DelSolr::Client.stubs(:new).with(settings.whitehall_solr).returns(:whitehall_client)
+
+    SolrWrapper.stubs(:new).with(:mainstream_client, anything, anything).returns(@solr)
+    SolrWrapper.stubs(:new).with(:whitehall_client, anything, anything).returns(@whitehall_solr)
   end
 end
