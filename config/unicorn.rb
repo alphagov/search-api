879818f07b86fde2921374f90d0cd1da4a0c20b1
David Heath <david.heath@digital.cabinet-office.gov.uk>
2012-08-08 18:10:46 +0100

    Fix unicorn config to pull in system-wide config
    The system wide config includes an important setting which will
    ensure the application and bundles reload correctly during a
    graceful restart.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,3 +1,6 @@
-app_root = File.dirname(File.dirname(__FILE__))
-working_directory app_root
+def load_file_if_exists(config, file)
+  config.instance_eval(File.read(file)) if File.exist?(file)
+end
+load_file_if_exists(self, "/etc/govuk/unicorn.rb")
+working_directory File.dirname(File.dirname(__FILE__))
 worker_processes 4
