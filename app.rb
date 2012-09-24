3d7e7a280de1fa02512d3ea7eda9e6e225f5a269
Tom Ward <tom@popdog.net>
2012-09-24 14:37:36 +0100

    Flatten namespaced routes. No changes other than indentation.

--- app.rb
+++ app.rb
@@ -44,168 +44,168 @@ before do
 end
 
 namespace settings.router[:path_prefix] || "" do
-  get "/search.?:format?" do
-    @query = params["q"].to_s.gsub(/[\u{0}-\u{1f}]/, "").strip
+get "/search.?:format?" do
+  @query = params["q"].to_s.gsub(/[\u{0}-\u{1f}]/, "").strip
 
-    if @query == ""
-      expires 3600, :public
-      @page_section = "Search"
-      @page_section_link = "/search"
-      @page_title = "Search | GOV.UK Beta (Test)"
-      return erb(:no_search_term)
-    end
-
-    expires 3600, :public if @query.length < 20
-
-    results = primary_search.search(@query, params["format_filter"])
-    secondary_results = secondary_search.search(@query)
-
-    logger.info "Found #{secondary_results.count} secondary results"
-
-    @secondary_results = secondary_results.take(5)
-    @more_secondary_results = secondary_results.length > 5
-    @results = results.take(50 - @secondary_results.length)
-    @total_results = @results.length + @secondary_results.length
-
-    if request.accept.include?("application/json") or params['format'] == 'json'
-      content_type :json
-      JSON.dump((@results + @secondary_results).map { |r| r.to_hash.merge(
-        highlight: r.highlight,
-        presentation_format: r.presentation_format,
-        humanized_format: r.humanized_format
-      ) })
-    else
-      @page_section = "Search"
-      @page_section_link = "/search"
-      @page_title = "#{@query} | Search | GOV.UK Beta (Test)"
-
-      headers SlimmerHeaders.headers(settings.slimmer_headers.merge(result_count: @results.length))
-
-      if @results.any?
-        erb(:search)
-      else
-        erb(:no_search_results)
-      end
-    end
+  if @query == ""
+    expires 3600, :public
+    @page_section = "Search"
+    @page_section_link = "/search"
+    @page_title = "Search | GOV.UK Beta (Test)"
+    return erb(:no_search_term)
   end
 
-  get "/preload-autocomplete" do
-    # Eventually this is likely to be a list of commonly searched for terms
-    # so searching for those is really fast. For the beta, this is just a list
-    # of all terms.
-    expires 86400, :public
+  expires 3600, :public if @query.length < 20
+
+  results = primary_search.search(@query, params["format_filter"])
+  secondary_results = secondary_search.search(@query)
+
+  logger.info "Found #{secondary_results.count} secondary results"
+
+  @secondary_results = secondary_results.take(5)
+  @more_secondary_results = secondary_results.length > 5
+  @results = results.take(50 - @secondary_results.length)
+  @total_results = @results.length + @secondary_results.length
+
+  if request.accept.include?("application/json") or params['format'] == 'json'
     content_type :json
-    results = primary_search.autocomplete_cache rescue []
-    JSON.dump(results.map { |r| r.to_hash })
-  end
-
-  get "/autocomplete" do
-    content_type :json
-    query = params['q']
-
-    unless query
-      expires 86400, :public
-      return '[]'
-    end
-
-    expires 3600, :public if query.length < 5
-
-    results = primary_search.complete(query, params["format_filter"]) rescue []
-    JSON.dump(results.map { |r| r.to_hash.merge(
+    JSON.dump((@results + @secondary_results).map { |r| r.to_hash.merge(
+      highlight: r.highlight,
       presentation_format: r.presentation_format,
       humanized_format: r.humanized_format
     ) })
-  end
+  else
+    @page_section = "Search"
+    @page_section_link = "/search"
+    @page_title = "#{@query} | Search | GOV.UK Beta (Test)"
 
-  get "/sitemap.xml" do
-    expires 86400, :public
-    # Site maps can have up to 50,000 links in them.
-    # We use one for / so we can have up to 49,999 others.
-    documents = primary_search.all_documents limit: 49_999
-    builder do |xml|
-      xml.instruct!
-      xml.urlset(xmlns: "http://www.sitemaps.org/schemas/sitemap/0.9") do
-        xml.url do
-  	xml.loc "#{base_url}#{"/"}"
-        end
-        documents.each do |document|
-  	xml.url do
-  	  url = document.link
-            url = "#{base_url}#{url}" if url =~ /^\//
-  	  xml.loc url
-  	end
-        end
-      end
-    end
-  end
+    headers SlimmerHeaders.headers(settings.slimmer_headers.merge(result_count: @results.length))
 
-  post "/documents" do
-    request.body.rewind
-    documents = [JSON.parse(request.body.read)].flatten.map { |hash|
-      Document.from_hash(hash)
-    }
-
-    boosts = {}
-    CSV.foreach(settings.boost_csv) { |row|
-      link, phrases = row
-      boosts[link] = phrases
-    }
-
-    better_documents = boost_documents(documents, boosts)
-
-    simple_json_result(primary_search.add(better_documents))
-  end
-
-  post "/commit" do
-    simple_json_result(primary_search.commit)
-  end
-
-  get "/documents/*" do
-    document = primary_search.get(params["splat"].first)
-    halt 404 unless document
-    content_type :json
-    JSON.dump document.to_hash
-  end
-
-  delete "/documents/*" do
-    simple_json_result(primary_search.delete(params["splat"].first))
-  end
-
-  post "/documents/*" do
-    def text_error(content)
-      halt 403, {"Content-Type" => "text/plain"}, content
-    end
-
-    unless request.form_data?
-      halt(
-        415,
-        {"Content-Type" => "text/plain"},
-        "Amendments require application/x-www-form-urlencoded data"
-      )
-    end
-    document = primary_search.get(params["splat"].first)
-    halt 404 unless document
-    text_error "Cannot change document links" if request.POST.include? 'link'
-
-    # Note: this expects application/x-www-form-urlencoded data, not JSON
-    request.POST.each_pair do |key, value|
-      begin
-        document.set key, value
-      rescue NoMethodError
-        text_error "Unrecognised field '#{key}'"
-      end
-    end
-    simple_json_result(primary_search.add([document]))
-  end
-
-  delete "/documents" do
-    if params['delete_all']
-      action = primary_search.delete_all
+    if @results.any?
+      erb(:search)
     else
-      action = primary_search.delete(params["link"])
+      erb(:no_search_results)
     end
-    simple_json_result(action)
   end
+end
+
+get "/preload-autocomplete" do
+  # Eventually this is likely to be a list of commonly searched for terms
+  # so searching for those is really fast. For the beta, this is just a list
+  # of all terms.
+  expires 86400, :public
+  content_type :json
+  results = primary_search.autocomplete_cache rescue []
+  JSON.dump(results.map { |r| r.to_hash })
+end
+
+get "/autocomplete" do
+  content_type :json
+  query = params['q']
+
+  unless query
+    expires 86400, :public
+    return '[]'
+  end
+
+  expires 3600, :public if query.length < 5
+
+  results = primary_search.complete(query, params["format_filter"]) rescue []
+  JSON.dump(results.map { |r| r.to_hash.merge(
+    presentation_format: r.presentation_format,
+    humanized_format: r.humanized_format
+  ) })
+end
+
+get "/sitemap.xml" do
+  expires 86400, :public
+  # Site maps can have up to 50,000 links in them.
+  # We use one for / so we can have up to 49,999 others.
+  documents = primary_search.all_documents limit: 49_999
+  builder do |xml|
+    xml.instruct!
+    xml.urlset(xmlns: "http://www.sitemaps.org/schemas/sitemap/0.9") do
+      xml.url do
+	xml.loc "#{base_url}#{"/"}"
+      end
+      documents.each do |document|
+	xml.url do
+	  url = document.link
+          url = "#{base_url}#{url}" if url =~ /^\//
+	  xml.loc url
+	end
+      end
+    end
+  end
+end
+
+post "/documents" do
+  request.body.rewind
+  documents = [JSON.parse(request.body.read)].flatten.map { |hash|
+    Document.from_hash(hash)
+  }
+
+  boosts = {}
+  CSV.foreach(settings.boost_csv) { |row|
+    link, phrases = row
+    boosts[link] = phrases
+  }
+
+  better_documents = boost_documents(documents, boosts)
+
+  simple_json_result(primary_search.add(better_documents))
+end
+
+post "/commit" do
+  simple_json_result(primary_search.commit)
+end
+
+get "/documents/*" do
+  document = primary_search.get(params["splat"].first)
+  halt 404 unless document
+  content_type :json
+  JSON.dump document.to_hash
+end
+
+delete "/documents/*" do
+  simple_json_result(primary_search.delete(params["splat"].first))
+end
+
+post "/documents/*" do
+  def text_error(content)
+    halt 403, {"Content-Type" => "text/plain"}, content
+  end
+
+  unless request.form_data?
+    halt(
+      415,
+      {"Content-Type" => "text/plain"},
+      "Amendments require application/x-www-form-urlencoded data"
+    )
+  end
+  document = primary_search.get(params["splat"].first)
+  halt 404 unless document
+  text_error "Cannot change document links" if request.POST.include? 'link'
+
+  # Note: this expects application/x-www-form-urlencoded data, not JSON
+  request.POST.each_pair do |key, value|
+    begin
+      document.set key, value
+    rescue NoMethodError
+      text_error "Unrecognised field '#{key}'"
+    end
+  end
+  simple_json_result(primary_search.add([document]))
+end
+
+delete "/documents" do
+  if params['delete_all']
+    action = primary_search.delete_all
+  else
+    action = primary_search.delete(params["link"])
+  end
+  simple_json_result(action)
+end
 end
 
 if settings.router[:path_prefix].empty?
