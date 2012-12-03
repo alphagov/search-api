d921506172ca9f2831526f4502f762f2eccbdd58
James Adam <james@lazyatom.com>
2012-12-03 11:01:39 +0000

    Remove GFR from the exception mailer configuration

--- config.rb
+++ config.rb
@@ -43,7 +43,7 @@ configure :production do
   if File.exist?("aws_secrets.yml")
     disable :show_exceptions
     use ExceptionMailer, YAML.load_file("aws_secrets.yml"),
-        to: ['govuk-exceptions@digital.cabinet-office.gov.uk', 'govuk@gofreerange.com'],
+        to: ['govuk-exceptions@digital.cabinet-office.gov.uk'],
         from: '"Winston Smith-Churchill" <winston@alphagov.co.uk>'
   end
 end
