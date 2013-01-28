9eeec0659bea6413d16fd71defc12b03a602642d
David Heath <david.heath@digital.cabinet-office.gov.uk>
2013-01-28 17:14:30 +0000

    Explicitly check status code after posting a new document
    Also refactored the tests to make their intention clearer.

--- test/integration/elasticsearch_indexing_test.rb
+++ test/integration/elasticsearch_indexing_test.rb
@@ -1,6 +1,7 @@
 require "integration_test_helper"
 require "app"
 require "rest-client"
+require "CGI"
 
 class ElasticsearchIndexingTest < IntegrationTest
 
@@ -8,53 +9,53 @@ class ElasticsearchIndexingTest < IntegrationTest
     use_elasticsearch_for_primary_search
     disable_secondary_search
     WebMock.disable_net_connect!(allow: "localhost:9200")
-  end
-
-  def test_should_send_a_document_to_elasticsearch_when_a_json_document_is_posted
-    reset_elasticsearch_index
-
-    test_data = {
+    @sample_document = {
       "title" => "TITLE",
       "description" => "DESCRIPTION",
       "format" => "answer",
       "link" => "/an-example-answer",
       "indexable_content" => "HERE IS SOME CONTENT"
     }
+  end
 
-    post "/documents", JSON.dump(test_data)
-    get "/documents/%2Fan-example-answer"
+  def retrieve_document_from_rummager(link)
+    get "/documents/#{CGI::escape(link)}"
+    JSON.parse(last_response.body)
+  end
 
-    parsed_response = JSON.parse(last_response.body)
+  def assert_document_is_in_rummager(document)
+    retrieved = retrieve_document_from_rummager(document['link'])
 
-    test_data.each do |key, value|
-      assert_equal value, parsed_response[key]
+    assert_equal document.keys.sort, retrieved.keys.sort
+
+    document.each do |key, value|
+      assert_equal value, retrieved[key], "Field #{key} should be '#{value}' but was '#{retrieved[key]}'"
     end
+  end
 
-    assert_equal test_data.keys.sort, parsed_response.keys.sort
+  def test_should_indicate_success_in_response_code_when_adding_a_new_document
+    reset_elasticsearch_index
+
+    post "/documents", JSON.dump(@sample_document)
+    assert last_response.ok?
+  end
+
+  def test_after_adding_a_document_to_index_should_be_able_to_retrieve_it_again
+    reset_elasticsearch_index
+
+    post "/documents", JSON.dump(@sample_document)
+
+    assert_document_is_in_rummager(@sample_document)
   end
 
   def test_should_be_able_to_index_a_document_with_additional_fields
     add_field_to_mappings("topics")
     reset_elasticsearch_index
 
-    test_data = {
-      "title" => "TITLE",
-      "description" => "DESCRIPTION",
-      "format" => "answer",
-      "link" => "/an-example-answer",
-      "indexable_content" => "HERE IS SOME CONTENT",
-      "topics" => [1,2]
-    }
+    test_data = @sample_document.merge("topics" => [1,2])
 
     post "/documents", JSON.dump(test_data)
-    get "/documents/%2Fan-example-answer"
 
-    parsed_response = JSON.parse(last_response.body)
-
-    test_data.each do |key, value|
-      assert_equal value, parsed_response[key]
-    end
-
-    assert_equal test_data.keys.sort, parsed_response.keys.sort
+    assert_document_is_in_rummager(test_data)
   end
 end
