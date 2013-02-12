3dcd0c4347def641eeaa3ef806b2535291971e78
Murray Steele <muz@h-lame.com>
2013-02-12 18:03:09 +0000

    Return a hash instead of an array, it's more intention revealing

--- test/functional/advanced_search_test.rb
+++ test/functional/advanced_search_test.rb
@@ -9,21 +9,21 @@ class AdvancedSearchTest < IntegrationTest
   end
 
   def test_returns_json_for_advanced_search_results
-    @backend_index.stubs(:advanced_search).returns([1, [sample_document]])
+    @backend_index.stubs(:advanced_search).returns({total: 1, results: [sample_document]})
     get "/meh/advanced_search", {per_page: '1', page: '1', keywords: 'meh'}, "HTTP_ACCEPT" => "application/json"
     assert_nothing_raised { MultiJson.decode(last_response.body) }
     assert_match /application\/json/, last_response.headers["Content-Type"]
   end
 
   def test_returns_json_when_requested_with_url_suffix
-    @backend_index.stubs(:advanced_search).returns([1, [sample_document]])
+    @backend_index.stubs(:advanced_search).returns({total: 1, results: [sample_document]})
     get "/meh/advanced_search.json", {per_page: '1', page: '1', keywords: 'meh'}
     assert_nothing_raised { MultiJson.decode(last_response.body) }
     assert_match /application\/json/, last_response.headers["Content-Type"]
   end
 
   def test_json_response_includes_total_and_results
-    @backend_index.stubs(:advanced_search).returns([1, [sample_document]])
+    @backend_index.stubs(:advanced_search).returns({total: 1, results: [sample_document]})
     get "/meh/advanced_search.json", {per_page: '1', page: '1', keywords: 'meh'}
     expected_result = {'total' => 1, 'results' => [sample_document_attributes.merge('highlight' => 'DESCRIPTION')]}
     assert_nothing_raised { MultiJson.decode(last_response.body) }
