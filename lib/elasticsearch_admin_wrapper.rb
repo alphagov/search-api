1554ea2ba1ade4789cee9d088dd53ed96c5ce717
Matthew Ford <matt@bitzesty.com>
2013-02-20 18:15:17 +0000

    wait_until_ready should wait for green
    This is because rerunning the tests will crash ES after a while - also
    use the default timeout of 30.

--- lib/elasticsearch_admin_wrapper.rb
+++ lib/elasticsearch_admin_wrapper.rb
@@ -42,6 +42,7 @@ class ElasticsearchAdminWrapper
     else
       @client.put("", index_payload.to_json)
       @logger.info "Index #{@client.index_name} created"
+      wait_until_ready
       return :created
     end
   end
@@ -83,7 +84,7 @@ class ElasticsearchAdminWrapper
   end
 
 private
-  def wait_until_ready(timeout=10)
+  def wait_until_ready(timeout=30)
     # Wait until the cluster is back up and running: useful when updating and
     # reopening an index.
 
@@ -96,7 +97,7 @@ private
     # back up.  So long as the primary is back up, we should be fine. See
     # <http://www.elasticsearch.org/guide/reference/api/admin-cluster-health.html>
 
-    health_params = { wait_for_status: "yellow", timeout: "#{timeout}s" }
+    health_params = { wait_for_status: "green", timeout: "#{timeout}s" }
     response = @client.get "/_cluster/health", params: health_params
     health = MultiJson.decode(response)
     if health["timed_out"] || ! ["green", "yellow"].include?(health["status"])
