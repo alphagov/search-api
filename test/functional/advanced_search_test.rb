9447a912fe49ba0caf4fceef0575c592ae7ff19b
David Thompson <david.thompson@digital.cabinet-office.gov.uk>
2013-03-08 12:14:50 +0000

    Make some search test failures more informative.

--- test/functional/advanced_search_test.rb
+++ test/functional/advanced_search_test.rb
@@ -11,7 +11,7 @@ class AdvancedSearchTest < IntegrationTest
     Elasticsearch::Index.any_instance.stubs(:advanced_search)
       .returns({total: 1, results: [sample_document]})
     get "/rummager_test/advanced_search", {per_page: '1', page: '1', keywords: 'meh'}, "HTTP_ACCEPT" => "application/json"
-    assert last_response.ok?
+    assert last_response.ok?, "Bad status: #{last_response.status}"
     result = MultiJson.decode(last_response.body)
     assert_match /application\/json/, last_response.headers["Content-Type"]
     assert_equal 1, result["total"]
@@ -22,7 +22,7 @@ class AdvancedSearchTest < IntegrationTest
       .returns({total: 1, results: [sample_document]})
     get "/rummager_test/advanced_search.json", {per_page: '1', page: '1', keywords: 'meh'}
     expected_result = {'total' => 1, 'results' => [sample_document_attributes.merge('highlight' => 'DESCRIPTION')]}
-    assert last_response.ok?
+    assert last_response.ok?, "Bad status: #{last_response.status}"
     assert_match /application\/json/, last_response.headers["Content-Type"]
     result = MultiJson.decode(last_response.body)
     assert_equal expected_result, result
