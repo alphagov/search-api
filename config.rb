1125fd13ee030f6620ac7ccbbaa82fee605868ed
mazz <mazz.mosley@digital.cabinet-office.gov.uk>
2013-04-26 14:37:52 +0000

    Set a default cache header for sitemaps

--- config.rb
+++ config.rb
@@ -7,6 +7,7 @@ set :search_config, SearchConfig.new
 set :default_index_name, "mainstream"
 
 set :static, true
+set :static_cache_control, [:public, :max_age => 86400]
 set :public_folder, File.join(File.dirname(__FILE__), 'public', 'system')
 
 configure :development do
