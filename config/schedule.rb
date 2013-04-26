e9c33083dd886f52d5d209fb36152522ff5cdfc9
Mazz <mazz.mosley@digital.cabinet-office.gov.uk>
2013-04-26 14:57:06 +0100

    Remove unnecessary RAKE_ENV

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
 set :output, {:error => 'log/cron.error.log', :standard => 'log/cron.log'}
-job_type :rake, 'cd :path && RACK_ENV=:environment /usr/local/bin/govuk_setenv search bundle exec rake :task'
+job_type :rake, 'cd :path && /usr/local/bin/govuk_setenv search bundle exec rake :task'
 
 every 1.day, :at => '1.00am' do
   rake 'sitemap:generate_and_replace'
