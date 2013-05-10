03eef08e3d5ac91f739ce2a879aba02d381f711a
David Thompson <david.thompson@digital.cabinet-office.gov.uk>
2013-05-10 15:17:03 +0100

    Run the generation task shortly past the hour.
    This sidesteps the problem where multiple boxes could potentially run on
    the cusp of the hour and generate different timestamps.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,9 @@
 set :output, {:error => 'log/cron.error.log', :standard => 'log/cron.log'}
 job_type :rake, 'cd :path && /usr/local/bin/govuk_setenv search bundle exec rake :task'
 
-every 1.day, :at => '1.00am' do
+# Sitemap filenames are generated based on the current day and hour. Putting
+# this at 10 past gets around any problems that might arise from running just
+# before the hour.
+every 1.day, :at => '1.10am' do
   rake 'sitemap:generate_and_replace'
 end
