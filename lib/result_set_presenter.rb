19560988f99a52faa5476519260200c70ad16e19
David Thompson <david.thompson@digital.cabinet-office.gov.uk>
2013-05-21 11:01:46 +0000

    Make ResultSetPresenter translate/present formats.

--- lib/result_set_presenter.rb
+++ lib/result_set_presenter.rb
@@ -15,11 +15,47 @@ class ResultSetPresenter
     )
   end
 
+  PRESENTATION_FORMAT_TRANSLATION = {
+    "planner" => "answer",
+    "smart_answer" => "answer",
+    "calculator" => "answer",
+    "licence_finder" => "answer",
+    "custom_application" => "answer",
+    "calendar" => "answer"
+  }
+
+  FORMAT_NAME_ALTERNATIVES = {
+    "programme" => "Benefits & credits",
+    "transaction" => "Services",
+    "local_transaction" => "Services",
+    "place" => "Services",
+    "answer" => "Quick answers",
+    "specialist_guidance" => "Specialist guidance"
+  }
+
 private
+  def presentation_format(document)
+    normalized = normalized_format(document)
+    PRESENTATION_FORMAT_TRANSLATION.fetch(normalized, normalized)
+  end
+
+  def humanized_format(document)
+    presentation = presentation_format(document)
+    FORMAT_NAME_ALTERNATIVES[presentation] || presentation.humanize.pluralize
+  end
+
+  def normalized_format(document)
+    if document.format
+      document.format.gsub("-", "_")
+    else
+      "unknown"
+    end
+  end
+
   def results
     @result_set.results.map { |r| r.to_hash.merge(
-      presentation_format: r.presentation_format,
-      humanized_format: r.humanized_format
+      presentation_format: presentation_format(r),
+      humanized_format: humanized_format(r)
     )}
   end
 end
--- test/unit/result_set_presenter_test.rb
+++ test/unit/result_set_presenter_test.rb
@@ -1,6 +1,7 @@
 require "test_helper"
 require "document"
 require "result_set_presenter"
+require "multi_json"
 
 class ResultSetPresenterTest < MiniTest::Unit::TestCase
 
@@ -19,25 +20,73 @@ class ResultSetPresenterTest < MiniTest::Unit::TestCase
     stub("result set", results: documents, total: 1)
   end
 
+  def single_result_with_format(format)
+    stub(results: [Document.new(FIELDS, :format => format)])
+  end
+
+  def output_for(presenter)
+    MultiJson.decode(presenter.present)
+  end
+
   def test_generates_json_from_documents
     presenter = ResultSetPresenter.new(result_set)
-    json = presenter.present
-    output = MultiJson.decode(json)
+    output = output_for(presenter)
     assert_equal 1, output.length
     assert_equal [], FIELDS - output[0].keys
   end
 
   def test_presented_json_includes_presentation_format
     presenter = ResultSetPresenter.new(result_set)
-    json = presenter.present
-    output = MultiJson.decode(json)
+    output = output_for(presenter)
     assert_equal "edition", output[0]["presentation_format"]
   end
 
   def test_presented_json_includes_humanized_format
     presenter = ResultSetPresenter.new(result_set)
-    json = presenter.present
-    output = MultiJson.decode(json)
+    output = output_for(presenter)
     assert_equal "Editions", output[0]["humanized_format"]
+  end
+
+  def test_should_use_answer_as_presentation_format_for_planner
+    result_set = single_result_with_format "planner"
+    output = output_for(ResultSetPresenter.new(result_set))
+    assert_equal "answer", output[0]["presentation_format"]
+  end
+
+  def test_should_use_answer_as_presentation_format_for_smart_answer
+    result_set = single_result_with_format "smart_answer"
+    output = output_for(ResultSetPresenter.new(result_set))
+    assert_equal "answer", output[0]["presentation_format"]
+  end
+
+  def test_should_use_answer_as_presentation_format_for_licence_finder
+    result_set = single_result_with_format "licence_finder"
+    output = output_for(ResultSetPresenter.new(result_set))
+    assert_equal "answer", output[0]["presentation_format"]
+  end
+
+  def test_should_use_guide_as_presentation_format_for_guide
+    result_set = single_result_with_format "guide"
+    output = output_for(ResultSetPresenter.new(result_set))
+    assert_equal "guide", output[0]["presentation_format"]
+  end
+
+  def test_should_use_humanized_format
+    result_set = single_result_with_format "place"
+    output = output_for(ResultSetPresenter.new(result_set))
+    assert_equal "Services", output[0]["humanized_format"]
+  end
+
+  def test_uses_presentation_format_to_find_alternative_format_name
+    presenter = ResultSetPresenter.new(single_result_with_format("foo"))
+    presenter.stubs(:presentation_format).returns("place")
+
+    assert_equal "Services", output_for(presenter)[0]["humanized_format"]
+  end
+
+  def test_generates_humanized_format_if_not_present
+    result_set = single_result_with_format "ocean_map"
+    output = output_for(ResultSetPresenter.new(result_set))
+    assert_equal "Ocean maps", output[0]["humanized_format"]
   end
 end
