5877889e8612c3dbbf4cf597fd9ebd816bb1a60c
David Thompson <david.thompson@digital.cabinet-office.gov.uk>
2013-05-24 10:09:37 +0100

    Pull cache functionality out of the org registry.

--- lib/organisation_registry.rb
+++ lib/organisation_registry.rb
@@ -1,37 +1,20 @@
-require "time"
+require "timed_cache"
 
 class OrganisationRegistry
   CACHE_LIFETIME = 12 * 3600  # 12 hours
 
-  def initialize(index, clock = DateTime)
+  def initialize(index, clock = Time)
     @index = index
-    @clock = clock
-    @cache_updated = nil
+    @cache = TimedCache.new(CACHE_LIFETIME, clock) { fetch }
   end
 
   def [](slug)
-    organisations.find { |o| o.link == "/government/organisations/#{slug}" }
+    @cache.get.find { |o| o.link == "/government/organisations/#{slug}" }
   end
 
 private
-  def organisations
-    if refresh_needed?
-      @organisations = fetch
-      @cache_updated = @clock.now
-    end
-    @organisations
-  end
-
   def fetch
     fields = %w{link title}
     @index.documents_by_format("organisation", fields: fields).to_a
-  end
-
-  def refresh_needed?
-    @organisations.nil? || @cache_updated.nil? || cache_age >= CACHE_LIFETIME
-  end
-
-  def cache_age
-    @clock.now - @cache_updated
   end
 end
