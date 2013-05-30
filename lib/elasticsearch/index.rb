5a1c5110c7785f83a83f8226103bcc30db3221ff
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2013-05-30 11:44:18 +0000

    Extract query building code into it's own class
    Yay for shrinking elasticsearch/index.rb

--- lib/elasticsearch/index.rb
+++ lib/elasticsearch/index.rb
@@ -9,6 +9,7 @@ require "elasticsearch/client"
 require "elasticsearch/escaping"
 require "elasticsearch/result_set"
 require "elasticsearch/scroll_enumerator"
+require "elasticsearch/search_query_builder"
 
 module Elasticsearch
   class Index
@@ -171,99 +172,8 @@ module Elasticsearch
     end
 
     def search(query, organisation=nil)
-      # Per-format boosting done as a filter, so the results get cached on the
-      # server, as they are the same for each query
-
-      boosted_formats = {
-        # Mainstream formats
-        "smart-answer"  => 1.5,
-        "transaction"   => 1.5,
-        # Inside Gov formats
-        "topical_event" => 1.5,
-        "minister"      => 1.7,
-        "organisation"  => 2.5,
-        "topic"         => 1.5,
-        "document_series" => 1.3,
-        "operational_field" => 1.5,
-      }
-
-      format_boosts = boosted_formats.map do |format, boost|
-        {
-          filter: { term: { format: format } },
-          boost: boost
-        }
-      end
-
-      # An implementation of http://wiki.apache.org/solr/FunctionQuery#recip
-      # Curve for 2 months: http://www.wolframalpha.com/share/clip?f=d41d8cd98f00b204e9800998ecf8427e5qr62u0si
-      #
-      # Behaves as a freshness boost for newer documents with a public_timestamp and search_format_types announcement
-      time_boost = {
-        filter: { term: { search_format_types: "announcement" } },
-        script: "((0.05 / ((3.16*pow(10,-11)) * abs(time() - doc['public_timestamp'].date.getMillis()) + 0.05)) + 0.12)"
-      }
-
-      query_analyzer = "query_default"
-
-      match_fields = {
-        "title" => 5,
-        "description" => 2,
-        "indexable_content" => 1,
-      }
-
-      # "driving theory test" => ["driving theory", "theory test"]
-      shingles = query.split.each_cons(2).map { |s| s.join(' ') }
-
-      # These boosts will be different on each query, so there's no benefit to
-      # caching them in a filter
-      shingle_boosts = shingles.map do |shingle|
-        match_fields.map do |field_name, _|
-          {
-            text: {
-              field_name => {
-                query: shingle,
-                type: "phrase",
-                boost: 2,
-                analyzer: query_analyzer
-              },
-            }
-          }
-        end
-      end
-
-      must_conditions = [
-        {
-          query_string: {
-            fields: match_fields.map { |name, boost|
-              boost == 1 ? name : "#{name}^#{boost}"
-            },
-            query: escape(query),
-            analyzer: query_analyzer
-          }
-        }
-      ]
-      if organisation
-        must_conditions << {
-          term: {
-            "organisations" => organisation
-          }
-        }
-      end
-
-      payload = {
-        from: 0, size: 50,
-        query: {
-          custom_filters_score: {
-            query: {
-              bool: {
-                must: must_conditions,
-                should: shingle_boosts
-              }
-            },
-            filters: format_boosts + [time_boost]
-          }
-        }
-      }.to_json
+      builder = SearchQueryBuilder.new(query, organisation)
+      payload = builder.query_hash.to_json
 
       logger.debug "Request payload: #{payload}"
       response = @client.get_with_payload("_search", payload)
