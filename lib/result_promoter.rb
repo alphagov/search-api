89681caa6b9d57dada3c525f65aa503578eea95a
David Heath <david.heath@digital.cabinet-office.gov.uk>
2013-06-07 15:36:09 +0100

    Allow decorating document hashes with promoted_for
    This is used at indexing time to set the promoted_for field

--- lib/result_promoter.rb
+++ lib/result_promoter.rb
@@ -1,12 +1,8 @@
 require 'promoted_result'
 
 class ResultPromoter
-  def initialize
-    @promoted_results = []
-  end
-
-  def add(link, terms)
-    @promoted_results << PromotedResult.new(link, terms)
+  def initialize(promoted_results)
+    @promoted_results = promoted_results
   end
 
   def promoted_terms_in(query)
@@ -14,6 +10,21 @@ class ResultPromoter
     terms.select do |term|
       @promoted_results.any? do |promoted_result|
         promoted_result.promoted_for?(term)
+      end
+    end
+  end
+
+  def with_promotion(document_hash)
+    promoted_terms = @promoted_results.select do |promoted_result|
+      document_hash["link"] == promoted_result.link
+    end.map do |promoted_result|
+      promoted_result.terms
+    end.flatten
+    document_hash.dup.tap do |new_hash|
+      if promoted_terms.any?
+        new_hash["promoted_for"] = promoted_terms.join(" ")
+      else
+        new_hash.delete("promoted_for")
       end
     end
   end
--- test/unit/result_promoter_test.rb
+++ test/unit/result_promoter_test.rb
@@ -3,12 +3,42 @@ require "result_promoter"
 
 class ResultPromoterTest < MiniTest::Unit::TestCase
   def test_can_identify_whether_a_query_contains_a_promoted_term
-    result_promoter = ResultPromoter.new
-    result_promoter.add("/jobsearch", ["job", "jobs"])
+    result_promoter = ResultPromoter.new([
+      PromotedResult.new("/jobsearch", ["job", "jobs"])
+    ])
+
     assert_equal ["jobs"], result_promoter.promoted_terms_in("jobs in birmingham")
     assert_equal [], result_promoter.promoted_terms_in("plumbing in birmingham")
     assert_equal ["job"], result_promoter.promoted_terms_in("job search")
     assert_equal ["job"], result_promoter.promoted_terms_in("job job")
     assert_equal [], result_promoter.promoted_terms_in("jobbing")
   end
+
+  def test_with_promotion_sets_promoted_for_for_promoted_documents
+    result_promoter = ResultPromoter.new([
+      PromotedResult.new("/jobsearch", ["job", "jobs"])
+    ])
+
+    promoted = result_promoter.with_promotion({"link" => "/jobsearch"})
+    assert_equal "job jobs", promoted["promoted_for"]
+  end
+
+  def test_with_promotion_does_not_set_promoted_for_for_other_documents
+    result_promoter = ResultPromoter.new([
+      PromotedResult.new("/jobsearch", ["job", "jobs"])
+    ])
+
+    promoted = result_promoter.with_promotion({"link" => "/tax-disc"})
+    refute_includes promoted, "promoted_for"
+  end
+
+  def test_with_promotion_clears_promoted_for_if_a_document_has_it_set_already
+    result_promoter = ResultPromoter.new([
+      PromotedResult.new("/jobsearch", ["job", "jobs"])
+    ])
+
+    promoted = result_promoter.with_promotion({"link" => "/tax-disc", "promoted_for" => "jobs"})
+    refute_includes promoted, "promoted_for"
+  end
+
 end
