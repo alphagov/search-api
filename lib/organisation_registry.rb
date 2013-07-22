dc7042bcac7efc62afdea12f9f1565d6b5b52909
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2013-07-22 15:12:44 +0000

    Organisations now have their acronyms indexed
    They are no longer included in the title and are indexed separately. See:
    https://github.com/alphagov/rummager/commit/97d354eab40031c8d0ad3a36b4b27fdb973f48b4
    https://github.com/alphagov/whitehall/commit/901e552e87b16adf14dfa8d056c8c16e6ab44827
    https://github.com/alphagov/whitehall/commit/428366cf7295568f2363342a5d33016f57422058

--- lib/organisation_registry.rb
+++ lib/organisation_registry.rb
@@ -32,41 +32,8 @@ private
     fields = %w{link title acronym organisation_type}
     organisations = @index.documents_by_format("organisation", fields: fields)
     organisations.map do |o|
-      fill_organisation_type(fix_acronym(o))
+      fill_organisation_type(o)
     end
-  end
-
-  def fix_acronym(organisation)
-    # If an organisation doesn't have an acronym specified, and if there is one
-    # in the title, extract it.
-    #
-    # HACK: this is to get around the smushing together of title and acronym in
-    # the current index. Once they are separate, this code can be removed.
-
-    if organisation.has_field?(:acronym) && organisation.acronym.nil?
-
-      # This regex extracts the last term in brackets as the "acronym" group;
-      # the remainder matches as the title, except the separating space.
-      #
-      # For example, "Ministry of Defence (MOD)" matches with:
-      #
-      #     "title" => "Ministry of Defence"
-      #     "acronym" => "MOD"
-      #
-      # Note that a title with multiple parenthesised suffixes will only match
-      # the last one. For example, "Forest Enterprise (England) (FEE)" becomes:
-      #
-      #     "title" => "Forest Enterprise (England)"
-      #     "acronym" => "FEE"
-      merged_pattern = %r{\A(?<title>.+) \((?<acronym>[^)]+)\)\Z}
-
-      pattern_match = merged_pattern.match(organisation.title)
-      if pattern_match
-        organisation.title = pattern_match["title"]
-        organisation.acronym = pattern_match["acronym"]
-      end
-    end
-    organisation
   end
 
   def fill_organisation_type(organisation)
