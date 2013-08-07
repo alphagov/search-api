84c836217e617d0bdde2d068624456798f4a516b
David Thompson <david.thompson@digital.cabinet-office.gov.uk>
2013-08-07 11:44:13 +0100

    Fix amend worker test.

--- test/unit/amend_worker_test.rb
+++ test/unit/amend_worker_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 require "elasticsearch/amend_worker"
 require "failed_job_worker"
 
-class DeleteWorkerTest < MiniTest::Unit::TestCase
+class AmendWorkerTest < MiniTest::Unit::TestCase
   def test_amends_documents
     mock_index = mock("index")
     mock_index.expects(:amend).with("/foobang", "title" => "New title")
@@ -12,5 +12,12 @@ class DeleteWorkerTest < MiniTest::Unit::TestCase
 
     worker = Elasticsearch::AmendWorker.new
     worker.perform("test-index", "/foobang", "title" => "New title")
+  end
+
+  def test_forwards_to_failure_queue
+    stub_message = {}
+    FailedJobWorker.expects(:perform_async).with(stub_message)
+    fail_block = Elasticsearch::AmendWorker.sidekiq_retries_exhausted_block
+    fail_block.call(stub_message)
   end
 end
