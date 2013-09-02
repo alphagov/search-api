6bfb1169925eda98422fe607a4c0aabd0a95add6
David Heath <david.heath@digital.cabinet-office.gov.uk>
2013-09-02 15:17:52 +0100

    Improve logging and send even sized chunks
    Send in chunks of 1MB

--- lib/bulk_loader.rb
+++ lib/bulk_loader.rb
@@ -1,13 +1,18 @@
+require 'time'
+
 class BulkLoader
   def initialize(search_config, index_name, options = {})
     @search_config = search_config
     @index_name = index_name
     @batch_size = options[:batch_size] || 1024
+    @logger = options[:logger] || Logger.new(nil)
   end
 
   def load_from(iostream)
     new_index = index_group.create_index
+    @logger.info "Created index #{new_index.real_name}"
     old_index = index_group.current_real
+    @logger.info "Old index #{old_index.real_name}"
 
     if old_index
       old_index.with_lock do
@@ -27,9 +32,33 @@ class BulkLoader
 
 private
   def do_indexing(index, iostream)
-    iostream.each_line.each_slice(@batch_size) do |lines|
+    @logger.info "Indexing to #{index.real_name}"
+    total_lines = 0
+    start_time = Time.now
+    in_even_sized_batches(iostream) do |lines|
       index.bulk_index(lines.join(""))
+      @logger.info "Sent #{lines.size} lines (#{byte_size(lines)} bytes)"
+      total_lines += lines.size
     end
+    elapsed_time = Time.now - start_time
+    @logger.info "Indexed %s lines in %.2f seconds (%.2f lines/sec)" % [total_lines, elapsed_time, total_lines / elapsed_time]
+  end
+
+  def byte_size(lines)
+    lines.inject(0) {|memo, l| memo + l.size}
+  end
+
+  def in_even_sized_batches(iostream, target_chunk_size = 1024*1024, &block)
+    chunk = []
+    iostream.each_line.each_slice(2) do |command, document|
+      chunk << command
+      chunk << document
+      if byte_size(chunk) > target_chunk_size
+        yield chunk
+        chunk = []
+      end
+    end
+    yield chunk if chunk.any?
   end
 
   def search_server
