d7da0f7909099c11a9c3d07ee653530a5d4c030b
Richard Boulton <richard.boulton@digital.cabinet-office.gov.uk>
2014-03-31 10:40:45 +0000

    Make fresh test data instead of copying a constant
    Previously, we used Marshal.load and Marshal.dump to get a copy of this
    data each time we used it, because the searcher code being tested
    mutates its input.  Instead, it's simpler to just generate fresh correct
    data each time.

--- test/unit/unified_searcher_test.rb
+++ test/unit/unified_searcher_test.rb
@@ -4,37 +4,39 @@ require "unified_searcher"
 
 class UnifiedSearcherTest < ShouldaUnitTestCase
 
-  DOCS = [{
-    "_index" => "government-2014-03-19t14:35:28z-a05cfc73-933a-41c7-adc0-309a715baf09",
-    _type: "edition",
-    _id: "/government/publications/staffordshire-cheese",
-    _score: 3.0514863,
-    "fields" => {
-      "description" => "Staffordshire Cheese Product of Designated Origin (PDO) and Staffordshire Organic Cheese.",
-      "title" => "Staffordshire Cheese",
-      "link" => "/government/publications/staffordshire-cheese",
-    },
-  }, {
-    "_index" => "mainstream-2014-03-19t14:35:28z-6472f975-dc38-49a5-98eb-c498e619650c",
-    _type: "edition",
-    _id: "/duty-relief-for-imports-and-exports",
-    _score: 0.49672604,
-    "fields" => {
-      "description" => "Schemes that offer reduced or zero rate duty and VAT for imports and exports",
-      "title" => "Duty relief for imports and exports",
-      "link" => "/duty-relief-for-imports-and-exports",
-    },
-  }, {
-    "_index" => "detailed-2014-03-19t14:35:27z-27e2831f-bd14-47d8-9c7a-3017e213efe3",
-    _type: "edition",
-    _id: "/dairy-farming-and-schemes",
-    _score: 0.34655035,
-    "fields" => {
-      "description" => "Information on hygiene standards and milking practices for UK dairy farmers, with a guide to EU schemes for dairy farmers and producers",
-      "title" => "Dairy farming and schemes",
-      "link" => "/dairy-farming-and-schemes",
-    },
-  }]
+  def sample_docs
+    [{
+      "_index" => "government-2014-03-19t14:35:28z-a05cfc73-933a-41c7-adc0-309a715baf09",
+      _type: "edition",
+      _id: "/government/publications/staffordshire-cheese",
+      _score: 3.0514863,
+      "fields" => {
+        "description" => "Staffordshire Cheese Product of Designated Origin (PDO) and Staffordshire Organic Cheese.",
+        "title" => "Staffordshire Cheese",
+        "link" => "/government/publications/staffordshire-cheese",
+      },
+    }, {
+      "_index" => "mainstream-2014-03-19t14:35:28z-6472f975-dc38-49a5-98eb-c498e619650c",
+      _type: "edition",
+      _id: "/duty-relief-for-imports-and-exports",
+      _score: 0.49672604,
+      "fields" => {
+        "description" => "Schemes that offer reduced or zero rate duty and VAT for imports and exports",
+        "title" => "Duty relief for imports and exports",
+        "link" => "/duty-relief-for-imports-and-exports",
+      },
+    }, {
+      "_index" => "detailed-2014-03-19t14:35:27z-27e2831f-bd14-47d8-9c7a-3017e213efe3",
+      _type: "edition",
+      _id: "/dairy-farming-and-schemes",
+      _score: 0.34655035,
+      "fields" => {
+        "description" => "Information on hygiene standards and milking practices for UK dairy farmers, with a guide to EU schemes for dairy farmers and producers",
+        "title" => "Dairy farming and schemes",
+        "link" => "/dairy-farming-and-schemes",
+      },
+    }]
+  end
 
   CHEESE_QUERY = {
     match: {
@@ -55,7 +57,7 @@ class UnifiedSearcherTest < ShouldaUnitTestCase
         query: CHEESE_QUERY,
         fields: UnifiedSearchBuilder::ALLOWED_RETURN_FIELDS,
       }).returns({
-        "hits" => {"hits" => Marshal.load(Marshal.dump(DOCS)), "total" => 3}
+        "hits" => {"hits" => sample_docs, "total" => 3}
       })
       @combined_index.expects(:index_name).returns(
         "mainstream,detailed,government"
@@ -91,7 +93,7 @@ class UnifiedSearcherTest < ShouldaUnitTestCase
         filter: {'exists' => {'field' => 'public_timestamp'}},
         sort: [{"public_timestamp" => {order: "asc"}}],
       }).returns({
-        "hits" => {"hits" => Marshal.load(Marshal.dump(DOCS)), "total" => 3}
+        "hits" => {"hits" => sample_docs, "total" => 3}
       })
       @combined_index.expects(:index_name).returns(
         "mainstream,detailed,government"
@@ -126,7 +128,7 @@ class UnifiedSearcherTest < ShouldaUnitTestCase
         filter: {"terms" => {"organisations" => ["ministry-of-magic"]}},
         fields: UnifiedSearchBuilder::ALLOWED_RETURN_FIELDS,
       }).returns({
-        "hits" => {"hits" => Marshal.load(Marshal.dump(DOCS)), "total" => 3}
+        "hits" => {"hits" => sample_docs, "total" => 3}
       })
       @combined_index.expects(:index_name).returns(
         "mainstream,detailed,government"
