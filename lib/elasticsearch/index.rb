5633dfde06bc05533f3d95b4bc799f367d5d6c32
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2014-05-08 15:55:14 +0100

    Whitespace.

--- lib/elasticsearch/index.rb
+++ lib/elasticsearch/index.rb
@@ -90,7 +90,7 @@ module Elasticsearch
         alias_info = MultiJson.decode(@client.get("_aliases"))
       rescue RestClient::ResourceNotFound => e
         response_body = MultiJson.decode(e.http_body)
-        if response_body['error'].start_with?("IndexMissingException") then 
+        if response_body['error'].start_with?("IndexMissingException") then
           return nil
         end
         raise
