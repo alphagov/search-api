2f6bbc976fe9e64a0a7feb2dab4d6789d73bba83
Richard Boulton <richard.boulton@digital.cabinet-office.gov.uk>
2014-06-04 23:30:58 +0000

    Remove old healthcheck code
    Remove support for scraping HTML results pages, and for accessing the
    "combined" search endpoint.

--- test/unit/health_check/json_search_client_test.rb
+++ test/unit/health_check/json_search_client_test.rb
@@ -6,37 +6,6 @@ Logging.logger.root.appenders = nil
 module HealthCheck
   class JsonSearchClientTest < ShouldaUnitTestCase
 
-    def combined_search_response_body
-      {
-        "streams" => {
-          "top-results" => {
-            "results" => [
-              {
-                "link" => "/a"
-              }
-            ]
-          },
-          "services-information" => {
-            "results" => [
-              {
-                "link" => "/b"
-              },
-              {
-                "link" => "/c"
-              }
-            ]
-          },
-          "departments-policy" => {
-            "results" => [
-              {
-                "link" => "/d"
-              }
-            ]
-          }
-        }
-      }
-    end
-
     def unified_search_response_body
       {
         "results" => [
@@ -50,30 +19,10 @@ module HealthCheck
       }
     end
 
-    def stub_combined_search(search_term, index="mainstream")
-      stub_request(:get, "http://www.gov.uk/api/search/govuk/search.json?q=#{CGI.escape(search_term)}").
-              with(headers: {'Accept'=>'*/*', 'User-Agent'=>'Ruby'}).
-              to_return(status: 200, body: combined_search_response_body.to_json)
-    end
-
     def stub_unified_search(search_term)
       stub_request(:get, "http://www.gov.uk/api/search.json?q=#{CGI.escape(search_term)}").
               with(headers: {'Accept'=>'*/*', 'User-Agent'=>'Ruby'}).
               to_return(status: 200, body: unified_search_response_body.to_json)
-    end
-
-    should "support combined search response format" do
-      stub_combined_search("cheese")
-      expected = ["/a", "/b", "/c"]
-      base_url = URI.parse("http://www.gov.uk/api/search/govuk/search.json")
-      assert_equal expected, JsonSearchClient.new(:base_url => base_url, index: "mainstream").search("cheese")
-    end
-
-    should "support combined search response format with an alternative index" do
-      stub_combined_search("cheese")
-      expected = ["/a", "/d"]
-      base_url = URI.parse("http://www.gov.uk/api/search/govuk/search.json")
-      assert_equal expected, JsonSearchClient.new(:base_url => base_url, index: "government").search("cheese")
     end
 
     should "support the unified search format" do
