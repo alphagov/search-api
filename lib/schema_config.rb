5467a008466f6735ac303a62b42d38749f15ab1e
Dominic Baggott <dominic.baggott@gmail.com>
2014-06-23 14:47:31 +0100

    Extract synonyms and stems to separate files

--- lib/schema_config.rb
+++ lib/schema_config.rb
@@ -16,17 +16,31 @@ private
   attr_reader :config_path
 
   def schema_yaml
-    YAML.load_file(File.join(
-      config_path,
-      "elasticsearch_schema.yml",
-    ))
+    load_yaml("elasticsearch_schema.yml")
   end
 
   def elasticsearch_index
-    schema_yaml["index"]
+    schema_yaml["index"].tap do |index|
+      index["settings"]["analysis"]["filter"].merge!(
+        "synonym" => synonym_filter,
+        "stemmer_override" => stems_filter,
+      )
+    end
   end
 
   def elasticsearch_mappings
     schema_yaml["mappings"]
+  end
+
+  def synonym_filter
+    load_yaml("synonyms.yml")
+  end
+
+  def stems_filter
+    load_yaml("stems.yml")
+  end
+
+  def load_yaml(file_path)
+    YAML.load_file(File.join(config_path, file_path))
   end
 end
