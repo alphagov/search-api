daf835d9e741b28de329228a2dc3871b9dac9d07
Dominic Baggott <dominic.baggott@gmail.com>
2014-07-04 12:06:52 +0100

    Make DeleteWorker#perform backwards compatible

--- lib/elasticsearch/delete_worker.rb
+++ lib/elasticsearch/delete_worker.rb
@@ -4,7 +4,13 @@ module Elasticsearch
   class DeleteWorker < BaseWorker
     forward_to_failure_queue
 
-    def perform(index_name, document_type, document_id)
+    def perform(index_name, document_type, document_id = nil)
+      # Handle previous method signature to cope with leftover queued jobs when
+      # we deploy.
+      if document_id.nil?
+        document_type, document_id = index(index_name).link_to_type_and_id(document_type)
+      end
+
       logger.info "Deleting #{document_type} document '#{document_id}' from '#{index_name}'"
       begin
         index(index_name).delete(document_type, document_id)
