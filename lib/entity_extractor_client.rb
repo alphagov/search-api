49c15ce6a8504b128e6776d5df2b49749b9a83c7
David Heath <david.heath@digital.cabinet-office.gov.uk>
2015-01-15 18:33:01 +0000

    Add timeout option and error handling
    all errors are logged but ignored silently

--- lib/entity_extractor_client.rb
+++ lib/entity_extractor_client.rb
@@ -3,19 +3,37 @@ require 'json'
 
 class EntityExtractorClient
   attr_reader :service_base_url
-  def initialize(service_base_url)
+
+  DEFAULT_OPEN_TIMEOUT_IN_SECONDS = 1
+  DEFAULT_READ_TIMEOUT_IN_SECONDS = 1
+
+  def initialize(service_base_url, options = {})
     @service_base_url = URI.parse(service_base_url)
+    @options = default_options.merge(options)
+    @logger = options[:logger] || Logging.logger[self]
   end
 
   def call(document)
-    response = RestClient.post(extract_url, document)
+    response = RestClient.post(extract_url, document, @options)
     JSON.parse(response)
+  rescue RestClient::Exception => e
+    logger.error(e)
+    nil
   end
 
 private
+  attr_reader :logger
+
   def extract_url
     service_base_url.clone.tap do |url|
       url.path = '/extract'
     end.to_s
+  end
+
+  def default_options
+    {
+      timeout: DEFAULT_READ_TIMEOUT_IN_SECONDS,
+      open_timeout: DEFAULT_OPEN_TIMEOUT_IN_SECONDS
+    }
   end
 end
--- test/unit/entity_extractor_client_test.rb
+++ test/unit/entity_extractor_client_test.rb
@@ -1,18 +1,32 @@
 require "test_helper"
 require "entity_extractor_client"
+require 'logger'
 
 class EntityExtractorClientTest < MiniTest::Unit::TestCase
+  def setup
+    @base_url = "http://localhost:3096"
+    @logstream = StringIO.new
+    @extractor = EntityExtractorClient.new(@base_url, logger: Logger.new(@logstream))
+  end
+
   def test_extract_calls_entity_extractor_service_and_deserialises_json_response
     document = "This is my document"
-    post_stub = stub_request(:post, "http://localhost:3096/extract")
+    stub_request(:post, "#{@base_url}/extract")
       .with(body: document)
       .to_return(
         status: 200,
         body: '["1"]'
       )
-    e = EntityExtractorClient.new("http://localhost:3096/")
-    response = e.call(document)
+    response = @extractor.call(document)
 
     assert_equal ["1"], response
+  end
+
+  def test_logs_message_and_returns_nil_on_timeout
+    stub_request(:post, "#{@base_url}/extract")
+      .with(body: "some text")
+      .to_timeout
+    assert_nil @extractor.call("some text")
+    assert_match /Request Timeout/, @logstream.string
   end
 end
