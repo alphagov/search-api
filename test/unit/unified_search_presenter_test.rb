a80e7be8e17bd21d7b6077425cb852322b647c30
Richard Boulton <richard.boulton@digital.cabinet-office.gov.uk>
2015-03-16 12:13:46 +0000

    Use IndexForSearch for the unified_search endpoint
    Updates ResultSetPresenter to use a new-style SchemaConfig object
    instead of taking elasticsearch mappings.  This is used to expand the
    values returned for fields for which allowed_values are defined.

--- test/unit/unified_search_presenter_test.rb
+++ test/unit/unified_search_presenter_test.rb
@@ -143,7 +143,7 @@ class UnifiedSearchPresenterTest < ShouldaUnitTestCase
       org_registry.nil? ? {} : {organisations: org_registry},
       options.fetch(:suggestions, []),
       options.fetch(:facet_examples, {}),
-      options.fetch(:mappings, {})
+      options.fetch(:schema, nil)
     )
   end
 
