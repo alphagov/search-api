4f0df694c476431af048c8016074a19de2c44b33
tijmen <tijmen@gmail.com>
2015-05-20 13:20:05 +0100

    Normalize and strip apostrophes when indexing/searching
    Currently, words with curly apostrophes (copy-pasted from a word
    document for example) are tokenised differently from words with
    "normal" apostrophes. This causes different documents to turn up
    depending on the type of apostrophe is used.
    
    This commit adds character filters to the analysers that first
    normalise the apostrophe and then remove it altogether in certain
    situations.

--- test/integration/settings_test.rb
+++ test/integration/settings_test.rb
@@ -3,37 +3,40 @@ require "integration_test_helper"
 class SettingsTest < IntegrationTest
   def test_default
     assert_tokenisation :default,
-      "It's A Small World" => ["it'", "small", "world"]
+      "It's A Small’s World" => ["it", "small", "world"],
+      "It's Mitt’s" => ["it", "mitt"]
   end
 
   def test_query_default
     assert_tokenisation :query_default,
-      "It's A Small World" => ["it'", "small", "world"]
+      "It's A Small World" => ["it", "small", "world"],
+      "It's, It’s Mr. O'Neill" => ["it", "it", "mr", "oneil"]
   end
 
   def test_shingled_query_analyzer
     assert_tokenisation :shingled_query_analyzer,
-      "Hello Hallo" => ["hello", "hello hallo", "hallo"]
+      "Hello Hallo" => ["hello", "hello hallo", "hallo"],
+      "H'lo ’Hallo" => ["h'lo", "h'lo hallo", "hallo"]
   end
 
   def test_exact_match
     assert_tokenisation :exact_match,
-      "It's A Small World" => ["it's a small world"]
+      "It’s A Small W'rld" => ["it's a small w'rld"]
   end
 
   def test_best_bet_stemmed_match
     assert_tokenisation :best_bet_stemmed_match,
-      "It's A Small World" => ["it'", "a", "small", "world"]
+      "It’s A Small W'rld" => ["it", "a", "small", "wrld"]
   end
 
   def test_spelling_analyzer
     assert_tokenisation :spelling_analyzer,
-      "Bi Grammed" => ["bi", "bi grammed", "grammed"]
+      "It’s Grammed" => ["its", "its grammed", "grammed"]
   end
 
   def test_string_for_sorting
     assert_tokenisation :string_for_sorting,
-      "It's A Small World" => ["it's a small world"]
+      "It's A Small W’rld" => ["its a small wrld"]
   end
 
   private
