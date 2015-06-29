52083c78ed000dcef042ccdabe5fd0bdddb1a709
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2015-06-29 14:15:07 +0100

    Fix expansion of specialist_sectors.
    They've been moved to the /topic namespace, so the code that inferred
    the slug from the link field was no longer working. Instead we now send
    the slug to rummager (alphagov/collections-publisher#112), so this
    inferrence is no longer necessary.

--- test/unit/specialist_sector_registry_test.rb
+++ test/unit/specialist_sector_registry_test.rb
@@ -11,8 +11,9 @@ class SpecialistSectorRegistryTest < MiniTest::Unit::TestCase
 
   def oil_and_gas
     Document.new(
-      sample_field_definitions(%w{link title}),
-      link: "/oil-and-gas/licensing",
+      sample_field_definitions(%w{link slug title}),
+      link: "/topic/oil-and-gas/licensing",
+      slug: "oil-and-gas/licensing",
       title: "Licensing"
     )
   end
@@ -22,13 +23,13 @@ class SpecialistSectorRegistryTest < MiniTest::Unit::TestCase
       .with("specialist_sector", anything)
       .returns([oil_and_gas])
     sector = @specialist_sector_registry["oil-and-gas/licensing"]
-    assert_equal oil_and_gas.link, sector["link"]
-    assert_equal oil_and_gas.title, sector["title"]
+    assert_equal oil_and_gas.link, sector.link
+    assert_equal oil_and_gas.title, sector.title
   end
 
   def test_only_required_fields_are_requested_from_index
     @index.expects(:documents_by_format)
-      .with("specialist_sector", sample_field_definitions(%w{link title}))
+      .with("specialist_sector", sample_field_definitions(%w{link slug title}))
       .returns([])
     @specialist_sector_registry["oil-and-gas/licensing"]
   end
@@ -39,18 +40,6 @@ class SpecialistSectorRegistryTest < MiniTest::Unit::TestCase
       .returns([oil_and_gas])
     sector = @specialist_sector_registry["foo"]
     assert_nil sector
-  end
-
-  def test_document_enumerator_is_traversed_only_once
-    document_enumerator = stub("enumerator")
-    document_enumerator.expects(:map).returns([
-      ["oil-and-gas/licensing", oil_and_gas],
-    ]).once
-    @index.stubs(:documents_by_format)
-      .with("specialist_sector", anything)
-      .returns(document_enumerator)
-      .once
-    assert @specialist_sector_registry["oil-and-gas/licensing"]
   end
 
   def test_uses_300_second_cache_lifetime
