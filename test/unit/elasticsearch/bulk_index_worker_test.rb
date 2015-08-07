cce43584df4bddf2725ae34e08d284f26066b17a
tijmen <tijmen@gmail.com>
2015-08-07 18:55:27 +0100

    Use constant for constant data

--- test/unit/elasticsearch/bulk_index_worker_test.rb
+++ test/unit/elasticsearch/bulk_index_worker_test.rb
@@ -4,21 +4,19 @@ require "elasticsearch/bulk_index_worker"
 require "elasticsearch/index"
 
 class BulkIndexWorkerTest < MiniTest::Unit::TestCase
-  def sample_document_hashes
-    %w(foo bar baz).map do |slug|
-      {:link => "/#{slug}", :title => slug.capitalize}
-    end
+  SAMPLE_DOCUMENT_HASHES = %w(foo bar baz).map do |slug|
+    { link: "/#{slug}", title: slug.capitalize }
   end
 
   def test_indexes_documents
     mock_index = mock("index")
-    mock_index.expects(:bulk_index).with(sample_document_hashes)
+    mock_index.expects(:bulk_index).with(SAMPLE_DOCUMENT_HASHES)
     Elasticsearch::SearchServer.any_instance.expects(:index)
       .with("test-index")
       .returns(mock_index)
 
     worker = Elasticsearch::BulkIndexWorker.new
-    worker.perform("test-index", sample_document_hashes)
+    worker.perform("test-index", SAMPLE_DOCUMENT_HASHES)
   end
 
   def test_retries_when_index_locked
@@ -31,10 +29,10 @@ class BulkIndexWorkerTest < MiniTest::Unit::TestCase
       .returns(mock_index)
 
     Elasticsearch::BulkIndexWorker.expects(:perform_in)
-      .with(lock_delay, "test-index", sample_document_hashes)
+      .with(lock_delay, "test-index", SAMPLE_DOCUMENT_HASHES)
 
     worker = Elasticsearch::BulkIndexWorker.new
-    worker.perform("test-index", sample_document_hashes)
+    worker.perform("test-index", SAMPLE_DOCUMENT_HASHES)
   end
 
   def test_forwards_to_failure_queue
