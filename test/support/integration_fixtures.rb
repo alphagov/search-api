23a0f51f1ed3b10c5fbe352bb929e1964df76dbf
tijmen <tijmen@gmail.com>
2015-08-07 18:33:29 +0100

    Use constant for test data
    There is no need for this data to be available as a method.

--- test/support/integration_fixtures.rb
+++ test/support/integration_fixtures.rb
@@ -1,17 +1,15 @@
 module IntegrationFixtures
   include Fixtures::DefaultMappings
 
-  def sample_document_attributes
-    {
-      "title" => "TITLE1",
-      "description" => "DESCRIPTION",
-      "format" => "local_transaction",
-      "section" => "life-in-the-uk",
-      "link" => "/URL"
-    }
-  end
+  SAMPLE_DOCUMENT_ATTRIBUTES = {
+    "title" => "TITLE1",
+    "description" => "DESCRIPTION",
+    "format" => "local_transaction",
+    "section" => "life-in-the-uk",
+    "link" => "/URL"
+  }
 
   def sample_document
-    Document.from_hash(sample_document_attributes, sample_document_types)
+    Document.from_hash(SAMPLE_DOCUMENT_ATTRIBUTES, sample_document_types)
   end
 end
