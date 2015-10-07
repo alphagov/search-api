bdad5a100bd5112308cf28d3b29969c03c0e153f
tijmen <tijmen@gmail.com>
2015-10-07 22:20:59 +0100

    Clean up ElasticsearchClosingTest
    This is a strange test, which was very liberal about which errors the
    app would raise. I don't think it's worth the code to support this in
    the test.

--- test/integration/elasticsearch_closing_test.rb
+++ test/integration/elasticsearch_closing_test.rb
@@ -4,27 +4,18 @@ class ElasticsearchClosingTest < IntegrationTest
 
   def setup
     stub_elasticsearch_settings
-    try_remove_test_index
+    create_test_index
   end
 
   def teardown
     clean_test_indexes
   end
 
-  # When the index is closed, we don't particularly mind which exception gets
-  # raised, so long as it's a client error (4xx) of some kind
-  def restclient_4xx_errors
-    RestClient::Exceptions::EXCEPTIONS_MAP.select { |code, exception|
-      (400..499).include? code
-    }.values
-  end
-
   def test_should_fail_to_insert_or_get_when_index_closed
-    create_test_index
-
     index = search_server.index_group(DEFAULT_INDEX_NAME).current
     index.close
-    assert_raises *(restclient_4xx_errors + [Elasticsearch::BulkIndexFailure]) do
+
+    assert_raises Elasticsearch::BulkIndexFailure do
       index.add([sample_document])
     end
   end
