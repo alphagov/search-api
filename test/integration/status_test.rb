877ad3b10be6f79646a08b83e18b2166152b76b6
tijmen <tijmen@gmail.com>
2015-10-07 21:40:40 +0100

    Move StatusTest from functional to integration
    We'd like to get rid of the functional tests. This is not a 100%
    integration test (it stubs Sidekiq), but that stubbing is unavoidable.

--- test/integration/status_test.rb
+++ test/integration/status_test.rb
@@ -1,21 +1,19 @@
-require "integration_test_helper"  # Because it tests the Sinatra app
+require "integration_test_helper"
 
 class StatusTest < IntegrationTest
   def test_shows_queue_job_count
     Sidekiq::Stats.any_instance.expects(:queues).returns(
-      {"bulk" => 12}
+      { "bulk" => 12 }
     )
+
     get "/_status"
+
     assert last_response.ok?
-    # Don't mind whether the response type has a charset
-    assert_equal "application/json", last_response.content_type.split(";")[0]
     assert_equal ["bulk"], parsed_response["queues"].keys
     assert_equal 12, parsed_response["queues"]["bulk"]["jobs"]
   end
 
   def test_shows_per_queue_retry_count
-    # Stubbing out Sidekiq's retries (SortedEntry instances)
-    # https://github.com/mperham/sidekiq/blob/v2.13.0/lib/sidekiq/api.rb#L203-248
     retries = %w(bulk bulk something-else).map { |q| stub(queue: q) }
 
     Sidekiq::RetrySet.expects(:new).returns(retries)
@@ -42,8 +40,6 @@ class StatusTest < IntegrationTest
   end
 
   def test_shows_per_queue_scheduled_count
-    # Stubbing out Sidekiq's schedule list (SortedEntry instances)
-    # https://github.com/mperham/sidekiq/blob/v2.13.0/lib/sidekiq/api.rb#L203-248
     scheduled = %w(bulk bulk something-else).map { |q| stub(queue: q) }
 
     Sidekiq::ScheduledSet.expects(:new).returns(scheduled)
