4dce8f7d7ea50153a37a44e3ebc1be8dddad88ed
tijmen <tijmen@gmail.com>
2015-10-07 22:28:14 +0100

    Remove Elasticsearch::Client#with_error_log_level
    This is not used but has significant gnarly test coverage.

--- test/unit/elasticsearch/client_test.rb
+++ test/unit/elasticsearch/client_test.rb
@@ -22,47 +22,6 @@ class ClientTest < MiniTest::Unit::TestCase
     end
   end
 
-  def test_changes_error_log_level
-    # Test we can change the log level for the duration of a block
-
-    RestClient::Request.expects(:execute).with(has_entry(method: :get))
-      .raises(internal_server_error)
-
-    Logging.logger[Elasticsearch::Client].expects(:warn)
-      .with(regexp_matches(/STUFF AND THINGS BROKE/))
-
-    client = Elasticsearch::Client.new("http://localhost/")
-    assert_raises RestClient::InternalServerError do
-      client.with_error_log_level(:warn) do
-        client.get("")
-      end
-    end
-  end
-
-  def test_resets_error_log_level
-    # Test that the error log level goes back to normal outside the block
-
-    RestClient::Request.expects(:execute).with(has_entry(method: :get))
-      .raises(internal_server_error)
-    RestClient::Request.expects(:execute).with(has_entry(method: :head))
-      .raises(internal_server_error("More breakage"))
-
-    Logging.logger[Elasticsearch::Client].expects(:warn)
-      .with(regexp_matches(/STUFF AND THINGS BROKE/))
-    Logging.logger[Elasticsearch::Client].expects(:error)
-      .with(regexp_matches(/More breakage/))
-
-    client = Elasticsearch::Client.new("http://localhost/")
-    assert_raises RestClient::InternalServerError do
-      client.with_error_log_level(:warn) do
-        client.get("")
-      end
-    end
-    assert_raises RestClient::InternalServerError do
-      client.head("")
-    end
-  end
-
   def test_timeout
     # Test that Elasticsearch::Client accepts a timeout option and passes it on
     # to RestClient
