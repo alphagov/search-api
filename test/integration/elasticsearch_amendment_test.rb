742d5281e4a41cf23802984730af13f905863641
tijmen <tijmen@gmail.com>
2015-10-07 21:32:54 +0100

    Remove redundant functional tests for amending
    - `test_should_amend_existing_document` is covered by the integration
    test.
    - `test_should_fail_on_json_post` tests that POST with JSON doesn't
    work. It's not clear what the problem was when this test was added (in
    2012), but this doesn't seem useful anymore.
    - `test_should_refuse_to_update_link` is covered by the integration
    test.
    
    The other tests have been converted to proper integration tests.

--- test/integration/elasticsearch_amendment_test.rb
+++ test/integration/elasticsearch_amendment_test.rb
@@ -25,6 +25,40 @@ class ElasticsearchAmendmentTest < IntegrationTest
     })
   end
 
+  def test_should_amend_a_document_queued
+    app.settings.enable_queue = true
+    commit_document("mainstream_test", {
+      "title" => "The old title",
+      "link" => "/an-example-answer",
+    })
+
+    post "/documents/%2Fan-example-answer", "title=A+new+title"
+
+    assert_equal 202, last_response.status
+    assert_document_is_in_rummager({
+      "title" => "A new title",
+      "link" => "/an-example-answer",
+    })
+  end
+
+  def test_rejects_unknown_fields
+    commit_document("mainstream_test", {
+      "title" => "The old title",
+      "link" => "/an-example-answer",
+    })
+
+    post "/documents/%2Fan-example-answer", "fish=Trout"
+
+    assert 403, last_response.status
+    assert_equal "Unrecognised field 'fish'", last_response.body
+  end
+
+  def test_returns_not_found_correctly
+    post "/documents/%2Fsome-non-existing-document", "title=A+new+title"
+
+    assert 404, last_response.status
+  end
+
   def test_should_fail_to_amend_link
     commit_document("mainstream_test", {
       "title" => "The title",
