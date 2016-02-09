f733dd201aaffee560b1710a4858f1505d3cdcce
Tijmen Brommet <tijmen@gmail.com>
2016-02-09 16:24:22 +0000

    Copy legacy "topics" to "policy areas"
    Policy areas used to be called "topics", which is an unfortunate naming
    collision with the concept of "topic" that lives under gov.uk/topic.
    
    We want to rename it to "policy areas". The plan is:
    
    - Always copy incoming topics to policy_areas
    - Request policy_areas instead of topics from whitehall/search
    - Use policy_areas in the payload sent to rummager
    - Remove the copying of topics to policy_areas
    - Remove the topics field
    
    This commit makes rummager copy the contents of `topics` in the payload
    to `policy_areas`.

--- test/unit/indexer/document_preparer_test.rb
+++ test/unit/indexer/document_preparer_test.rb
@@ -24,5 +24,20 @@ describe Indexer::DocumentPreparer do
         assert_nil updated_doc_hash['alpha_taxonomy']
       end
     end
+
+    describe "policy areas migration" do
+      it "copies topics to policy areas" do
+        doc_hash = {
+          "link" => "/some-link",
+          "topics" => ["a", "b"],
+        }
+        stub_request(:get, "http://contentapi.dev.gov.uk/some-link.json").
+          to_return(:status => 404, :body => "", :headers => {})
+
+        updated_doc_hash = Indexer::DocumentPreparer.new("fake_client").prepared(doc_hash, nil, true)
+
+        assert_equal ["a", "b"], updated_doc_hash["policy_areas"]
+      end
+    end
   end
 end
