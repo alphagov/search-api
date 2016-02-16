aff2505628beb1b6587a5435b074fc3e7ed14844
Tijmen Brommet <tijmen@gmail.com>
2016-02-16 13:47:39 +0000

    Extract message processing into method
    This makes it easier to read and makes it easier to rescue errors.

--- lib/index_documents.rb
+++ lib/index_documents.rb
@@ -14,20 +14,23 @@ class IndexDocuments
   }
 
   def process(message)
-    if publishing_app_migrated?(message)
-      raw_links = links_from_payload(message)
-
-      unless raw_links.empty?
-        links = rummager_fields_from_links(raw_links)
-        base_path = document_base_path(message)
-        update_index(base_path, links)
-      end
-    end
-
+    index_links_from_message(message)
     message.ack
   end
 
 private
+
+  def index_links_from_message(message)
+    return unless publishing_app_migrated?(message)
+    raw_links = links_from_payload(message)
+
+    unless raw_links.empty?
+      links = rummager_fields_from_links(raw_links)
+      base_path = document_base_path(message)
+      update_index(base_path, links)
+    end
+  end
+
   def publishing_app_migrated?(message)
     MIGRATED_TAGGING_APPS.include? message.payload["publishing_app"]
   end
