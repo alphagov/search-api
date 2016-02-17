25e35668e8438162cd7b1cf8e1c3a367229a17b4
Mo Baig <mo@mobaig.com>
2016-02-17 15:59:33 +0000

    Remove all code related to indexing taxonomy mappings
    This was a spike to get taxonomy data into the search API. This approach
    has since been superseded by the creation of dedicated taxon content
    items that are tagged via the links hash.

--- test/unit/indexer/document_preparer_test.rb
+++ test/unit/indexer/document_preparer_test.rb
@@ -3,28 +3,6 @@ require "indexer"
 
 describe Indexer::DocumentPreparer do
   describe "#prepared" do
-    describe "alpha taxonomies" do
-      before do
-        Indexer::TagLookup.stubs(:prepare_tags).returns({ "link" => "some-slug" })
-      end
-
-      it "adds an alpha taxonomy to the doc if a match is found" do
-        ::TaxonomyPrototype::TaxonFinder.stubs(:find_by).returns(["taxon-1", "taxon-2"])
-
-        updated_doc_hash = Indexer::DocumentPreparer.new("fake_client").prepared({}, nil, true)
-
-        assert_equal ["taxon-1", "taxon-2"], updated_doc_hash['alpha_taxonomy']
-      end
-
-      it "does nothing to the doc if no match is found" do
-        ::TaxonomyPrototype::TaxonFinder.stubs(:find_by).returns(nil)
-
-        updated_doc_hash = Indexer::DocumentPreparer.new("fake_client").prepared({}, nil, true)
-
-        assert_nil updated_doc_hash['alpha_taxonomy']
-      end
-    end
-
     describe "policy areas migration" do
       it "copies topics to policy areas" do
         doc_hash = {
