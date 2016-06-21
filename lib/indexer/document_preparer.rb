0dcee4364775881a808775d86b90ad3bfe9dffd1
Mo Baig <mo@mobaig.com>
2016-06-21 14:52:31 +0100

    Add Airbrake warning if unexpected links are present during indexing
    Rummager now fetches links data directly from the publishing API when
    indexing a document. We're in the process of updating Rummager client
    apps to not send mainstream browse pages, specialist sectors and
    organisation links.
    
    This commit adds an Airbrake message if links of
    this type are encountered when preparing a document for indexing.

--- lib/indexer/document_preparer.rb
+++ lib/indexer/document_preparer.rb
@@ -6,6 +6,7 @@ module Indexer
     end
 
     def prepared(doc_hash, popularities, is_content_index)
+      warn_if_links_present_in(doc_hash)
       if is_content_index
         doc_hash = copy_legacy_topic_to_policy_area(doc_hash)
         doc_hash = prepare_popularity_field(doc_hash, popularities)
@@ -19,6 +20,16 @@ module Indexer
     end
 
   private
+
+    class UnexpectedLinksError < StandardError
+    end
+
+    def warn_if_links_present_in(doc_hash)
+      unexpected_links = %w{ mainstream_browse_pages organisations specialist_sectors }
+      if doc_hash.keys.any? { |key| key.in?(unexpected_links) }
+        Airbrake.notify_or_ignore(UnexpectedLinksError.new, parameters: doc_hash)
+      end
+    end
 
     def prepare_popularity_field(doc_hash, popularities)
       pop = 0.0
