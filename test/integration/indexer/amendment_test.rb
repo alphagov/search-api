3cefda9746f7a4d7074f69ccaa5ff17b4c037ffa
Tijmen Brommet <tijmen@gmail.com>
2016-07-05 13:45:02 +0100

    Remove spurious error handling in index endpoint
    This endpoint handles three exceptions/validations:
    
    - Not providing form-data. This was added before 2012 and I assume that
    we no longer need this validation. All apps send their data with the
    proper API calls. Removing this will remove complexity.
    - Rescuing `ArgumentError`. This was added when this endpoint was still
    synchronous. The AmendWorker will never raise a `ArgumentError` when
    used correctly, so this can be removed.
    - Rescuing `SearchIndices::DocumentNotFound`. This also comes from the
    time when this endpoint was synchronous. Since
    `AmendWorker#perform_async` will never raise this error we can remove
    the code.
    
    Reference:
    https://github.com/alphagov/rummager/commit/b24ec9a02267a666e1a44e26807e
    d2dbc17067ea

--- test/integration/indexer/amendment_test.rb
+++ test/integration/indexer/amendment_test.rb
@@ -51,36 +51,4 @@ class ElasticsearchAmendmentTest < IntegrationTest
 
     assert_equal 202, last_response.status
   end
-
-  def test_rejects_unknown_fields
-    commit_document("mainstream_test", {
-      "title" => "The old title",
-      "link" => "/an-example-answer",
-    })
-
-    post "/documents/%2Fan-example-answer", "fish=Trout"
-
-    assert 403, last_response.status
-    assert_equal "Unrecognised field 'fish'", last_response.body
-  end
-
-  def test_returns_not_found_correctly
-    post "/documents/%2Fsome-non-existing-document", "title=A+new+title"
-
-    assert 404, last_response.status
-  end
-
-  def test_should_fail_to_amend_link
-    commit_document("mainstream_test", {
-      "title" => "The title",
-      "link" => "/an-example-answer",
-    })
-
-    post "/documents/%2Fan-example-answer", "link=/wibble"
-
-    assert_document_is_in_rummager({
-      "title" => "The title",
-      "link" => "/an-example-answer",
-    })
-  end
 end
