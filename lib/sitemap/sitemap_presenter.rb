1b0d85ddc9e4500b6a52a90f03834b0d8d3a42d7
Suzanne Hamilton <suzanne.hamilton@digital.cabinet-office.gov.uk>
2017-07-31 12:39:16 +0100

    Extract presenter for generating sitemap data for a document
    Refactor the sitemap generation code to split the XML formatting from
    the code which converts document data to sitemap field data.

---
+++ lib/sitemap/sitemap_presenter.rb
@@ -0,0 +1,38 @@
+class SitemapPresenter
+  def initialize(document)
+    @document = document
+    @logger = Logging.logger[self]
+  end
+
+  def url
+    if document.link.start_with?("http")
+      document.link
+    else
+      URI.join(base_url, document.link)
+    end
+  end
+
+  def last_updated
+    return nil unless document.public_timestamp
+
+    # Attempt to parse timestamp to validate it
+    DateTime.iso8601(document.public_timestamp)
+    document.public_timestamp
+  rescue ArgumentError
+    @logger.warn("Invalid timestamp '#{document.public_timestamp}' for page '#{document.link}'")
+    # Ignore invalid or missing timestamps
+    nil
+  end
+
+  def priority
+    document.is_withdrawn ? 0.25 : 1
+  end
+
+private
+
+  attr_reader :document
+
+  def base_url
+    Plek.current.website_root
+  end
+end
