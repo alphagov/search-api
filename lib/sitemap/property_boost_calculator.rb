0860e4b76a385ca814b3c6503104f22fa79f0f0e
Suzanne Hamilton <suzanne.hamilton@digital.cabinet-office.gov.uk>
2017-08-02 10:55:52 +0100

    Apply more boosts to page priorities in the sitemap
    Internal search results are upweighted or downweighted based on several
    properties of a document, such as whether it is guidance or whether it
    is marked as historic (i.e. it is a publication by a previous
    government).
    
    This commit applies the same boosts to *external* search by using them
    as a multiplier in the sitemap priorities. This should improve the
    relative rankings of GOV.UK pages in external search results, helping
    users to find the information that they're more likely to be searching
    for.
    
    https://trello.com/c/wTcs0JaG/194-add-format-weightings-to-sitemap

--- lib/sitemap/property_boost_calculator.rb
+++ lib/sitemap/property_boost_calculator.rb
@@ -1,24 +1,34 @@
 class PropertyBoostCalculator
   def initialize
-    boost_config = YAML.load_file('config/query/boosting.yml')
-    @format_boosts = boost_config["format"]
+    @boost_config = YAML.load_file('config/query/boosting.yml')
+    @max_boosts = calculate_max_boosts
   end
 
   def boost(document)
-    page_boost = @format_boosts[document.format] || 1
+    boost_values = @boost_config.map do |property, boosts|
+      if document.has_field?(property) && boosts[document.get(property)]
+        page_boost = boosts[document.get(property)]
+      else
+        page_boost = 1
+      end
 
-    # Normalise format boost to always give a value between 0 and 1
-    (page_boost.to_f / max_boost).round(2)
+      # Normalise format boost to always give a value between 0 and 1
+      page_boost.to_f / @max_boosts[property]
+    end
+
+    boost_values.inject(:*).round(2)
   end
 
 private
 
-  def max_boost
-    @_max_boost ||= calculate_max_boost
-  end
+  def calculate_max_boosts
+    @boost_config.keys.each_with_object({}) do |property, max_boosts|
+      boosts = @boost_config[property]
 
-  def calculate_max_boost
-    max_configured_boost = @format_boosts.values.max
-    max_configured_boost >= 1 ? max_configured_boost : 1
+      max_configured_boost = boosts.values.max
+      max_boost = max_configured_boost >= 1 ? max_configured_boost : 1
+
+      max_boosts[property] = max_boost
+    end
   end
 end
--- test/unit/sitemap/property_boost_calculator_test.rb
+++ test/unit/sitemap/property_boost_calculator_test.rb
@@ -40,6 +40,25 @@ class PropertyBoostCalculatorTest < Minitest::Test
     assert_equal expected_default_boost, calculator.boost(build_document(format: nil))
   end
 
+  def test_document_without_property_has_default_boost
+    max_boost = 2.5
+
+    stub_boost_config({
+      "some_property" => {
+        "some_value" => max_boost,
+      }
+    })
+
+    field_definitions = {}
+    attributes = {}
+    document_without_property = Document.new(field_definitions, attributes)
+
+    calculator = PropertyBoostCalculator.new
+
+    expected_default_boost = 1 / max_boost
+    assert_equal expected_default_boost, calculator.boost(document_without_property)
+  end
+
   def test_default_is_1_if_configured_properties_are_all_downweighted
     stub_boost_config({
       "format" => {
@@ -53,6 +72,33 @@ class PropertyBoostCalculatorTest < Minitest::Test
 
     assert_equal 0.2, calculator.boost(build_document(format: "organisation"))
     assert_equal 1, calculator.boost(build_document(format: "other_format"))
+  end
+
+  def test_boosts_for_different_fields_are_multiplied
+    stub_boost_config({
+      "format" => {
+        "organisation" => 2,
+        "publication" => 1,
+      },
+      "content_store_document_type" => {
+        "foi_release" => 0.2
+      },
+      "navigation_document_supertype" => {
+        "guidance" => 2.5
+      }
+    })
+
+    calculator = PropertyBoostCalculator.new
+
+    document = Document.new(sample_field_definitions, {
+      "format" => "publication",
+      "content_store_document_type" => "foi_release",
+      "navigation_document_supertype" => "other"
+    })
+
+    # format boost * document type boost * navigation supertype boost
+    # (1/2) * 0.2 * (1/2.5) = 0.04
+    assert_equal 0.04, calculator.boost(document)
   end
 
   def test_boosts_are_not_rounded_by_integer_division
