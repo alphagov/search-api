b9ae8aaf25509f56e55b2cf73c6bf2adbf42278a
brenetic <b@brenetic.com>
2017-08-15 11:58:02 +0000

    Add unit test for deletion of valid document
    This was previous part of https://github.com/alphagov/rummager/pull/864 which is closed.
    However it is still a useful test.

--- test/unit/govuk_index/elasticsearch_processor_test.rb
+++ test/unit/govuk_index/elasticsearch_processor_test.rb
@@ -4,7 +4,7 @@ class ElasticsearchProcessorTest < Minitest::Test
   def test_should_save_valid_document
     presenter = stub(:presenter)
     presenter.stubs(:identifier).returns(
-      _type: "cheddar",
+      _type: "help_page",
       _id: "/cheese"
     )
     presenter.stubs(:document).returns(
@@ -18,6 +18,33 @@ class ElasticsearchProcessorTest < Minitest::Test
 
     actions = GovukIndex::ElasticsearchProcessor.new
     actions.save(presenter)
+    actions.commit
+  end
+
+  def test_should_delete_valid_document
+    presenter = stub(:presenter)
+    presenter.stubs(:identifier).returns(
+      _type: "help_page",
+      _id: "/cheese"
+    )
+    presenter.stubs(:document).returns(
+      link: "/cheese",
+      title: "We love cheese"
+    )
+
+    client = stub('client')
+    Services.stubs('elasticsearch').returns(client)
+    client.expects(:bulk).with(
+      index: SearchConfig.instance.govuk_index_name,
+      body: [
+        {
+          delete: presenter.identifier
+        }
+      ]
+    )
+
+    actions = GovukIndex::ElasticsearchProcessor.new
+    actions.delete(presenter)
     actions.commit
   end
 end
