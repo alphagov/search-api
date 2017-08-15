d0fe21a9b5ef01d2dbaacd3101d96c2086f62304
David Henry <david@decoybecoy.com>
2017-08-15 22:15:13 +0100

    These specs need to be manually reset

---
+++ spec/unit/search/query_builder_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+RSpec.describe 'QueryBuilderTest', tags: ['shoulda'] do
+  before do
+    Search::BestBetsChecker.any_instance.stubs best_bets: [], worst_bets: []
+    IndexHelpers.stub_elasticsearch_settings
+  end
+
+  context "with a simple search query" do
+    it "return a correct query object" do
+      builder = builder_with_params(start: 11, count: 34, return_fields: ['a_field'])
+
+      result = builder.payload
+      assert_equal 11, result[:from]
+      assert_equal 34, result[:size]
+      assert result[:fields].include?('a_field')
+      assert result.key?(:query)
+    end
+  end
+
+  context "more like this" do
+    it "call the payload for a more like this query" do
+      builder = builder_with_params(similar_to: %{"/hello-world"})
+
+      builder.expects(:more_like_this_query_hash).once
+
+      # TODO: assert what the payload looks like
+      builder.payload
+    end
+  end
+
+  def builder_with_params(params)
+    Search::QueryBuilder.new(
+      search_params: Search::QueryParameters.new({ filters: [] }.merge(params)),
+      content_index_names: SearchConfig.instance.content_index_names,
+      metasearch_index: SearchConfig.instance.metasearch_index
+    )
+  end
+end
