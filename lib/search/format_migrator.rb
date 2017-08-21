7a4352affbfde4b0a6496524117be8e9a216834b
Rosa Fox <rosa.fox@digital.cabinet-office.gov.uk>
2017-08-21 14:58:44 +0000

    Add help_page to migrated formats

--- lib/search/format_migrator.rb
+++ lib/search/format_migrator.rb
@@ -1,7 +1,7 @@
 module Search
   class FormatMigrator
     def self.migrated_formats
-      @migrated_formats ||= YAML.load_file(File.join(__dir__, '../../config/govuk_index/migrated_formats.yaml'))['formats']
+      @migrated_formats ||= YAML.load_file(File.join(__dir__, '../../config/govuk_index/migrated_formats.yaml'))
     end
 
     def initialize(base)
