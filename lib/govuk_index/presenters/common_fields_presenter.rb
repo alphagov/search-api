b5fd1b6f87766061358d4a5a87c7a73b5906f31e
brenetic <b@brenetic.com>
2017-08-23 14:34:31 +0000

    Cater for documents with parts
    Formats such as `guides` have parts in their details hash. We are now
    able to index these as well.
    
    Also refactor the presenters. A little bit.

--- lib/govuk_index/presenters/common_fields_presenter.rb
+++ lib/govuk_index/presenters/common_fields_presenter.rb
@@ -1,42 +1,72 @@
 module GovukIndex
   class CommonFieldsPresenter
-    def initialize(payload, sanitiser)
+    def initialize(payload)
       @payload = payload
-      @sanitiser = sanitiser
     end
 
-    def present
-      {
-        content_id: payload["content_id"],
-        content_store_document_type: payload["document_type"],
-        description: payload["description"],
-        email_document_supertype: payload["email_document_supertype"],
-        format: payload["document_type"],
-        government_document_supertype: payload["government_document_supertype"],
-        indexable_content: sanitiser.clean(payload),
-        is_withdrawn: withdrawn?,
-        link: payload["base_path"],
-        navigation_document_supertype: payload["navigation_document_supertype"],
-        popularity: calculate_popularity,
-        public_timestamp: payload["public_updated_at"],
-        publishing_app: payload["publishing_app"],
-        rendering_app: payload["rendering_app"],
-        title: payload["title"],
-        user_journey_document_supertype: payload["user_journey_document_supertype"],
-      }
+    def content_id
+      payload["content_id"]
+    end
+
+    def content_store_document_type
+      payload["document_type"]
+    end
+
+    def description
+      payload["description"]
+    end
+
+    def email_document_supertype
+      payload["email_document_supertype"]
+    end
+
+    def format
+      payload["document_type"]
+    end
+
+    def government_document_supertype
+      payload["government_document_supertype"]
+    end
+
+    def is_withdrawn
+      !payload["withdrawn_notice"].nil?
+    end
+
+    def link
+      payload["base_path"]
+    end
+
+    def navigation_document_supertype
+      payload["navigation_document_supertype"]
+    end
+
+    def popularity
+      lookup = Indexer::PopularityLookup.new("govuk_index", SearchConfig.instance)
+      lookup.lookup_popularities([payload["base_path"]])[payload["base_path"]]
+    end
+
+    def public_timestamp
+      payload["public_updated_at"]
+    end
+
+    def publishing_app
+      payload["publishing_app"]
+    end
+
+    def rendering_app
+      payload["rendering_app"]
+    end
+
+    def title
+      payload["title"]
+    end
+
+    def user_journey_document_supertype
+      payload["user_journey_document_supertype"]
     end
 
   private
 
-    attr_reader :payload, :sanitiser
-
-    def withdrawn?
-      !payload["withdrawn_notice"].nil?
-    end
-
-    def calculate_popularity
-      lookup = Indexer::PopularityLookup.new("govuk_index", SearchConfig.instance)
-      lookup.lookup_popularities([payload["base_path"]])[payload["base_path"]]
-    end
+    attr_reader :payload
   end
 end
--- lib/govuk_index/presenters/elasticsearch_presenter.rb
+++ lib/govuk_index/presenters/elasticsearch_presenter.rb
@@ -15,8 +15,33 @@ module GovukIndex
     end
 
     def document
-      {}.merge(CommonFieldsPresenter.new(payload, IndexableContentSanitiser.new).present)
-        .merge(ExpandedLinksPresenter.new(payload["expanded_links"]).present)
+      {
+        content_id:                         common_fields.content_id,
+        content_store_document_type:        common_fields.content_store_document_type,
+        description:                        common_fields.description,
+        email_document_supertype:           common_fields.email_document_supertype,
+        format:                             common_fields.format,
+        government_document_supertype:      common_fields.government_document_supertype,
+        is_withdrawn:                       common_fields.is_withdrawn,
+        link:                               common_fields.link,
+        navigation_document_supertype:      common_fields.navigation_document_supertype,
+        popularity:                         common_fields.popularity,
+        public_timestamp:                   common_fields.public_timestamp,
+        publishing_app:                     common_fields.publishing_app,
+        rendering_app:                      common_fields.rendering_app,
+        title:                              common_fields.title,
+        user_journey_document_supertype:    common_fields.user_journey_document_supertype,
+        mainstream_browse_pages:            expanded_links.mainstream_browse_pages,
+        mainstream_browse_page_content_ids: expanded_links.mainstream_browse_page_content_ids,
+        organisations:                      expanded_links.organisations,
+        organisation_content_ids:           expanded_links.organisation_content_ids,
+        part_of_taxonomy_tree:              expanded_links.part_of_taxonomy_tree,
+        primary_publishing_organisation:    expanded_links.primary_publishing_organisation,
+        specialist_sectors:                 expanded_links.specialist_sectors,
+        taxons:                             expanded_links.taxons,
+        topic_content_ids:                  expanded_links.topic_content_ids,
+        indexable_content:                  indexable_content.indexable_content
+      }
     end
 
     def base_path
@@ -31,5 +56,17 @@ module GovukIndex
   private
 
     attr_reader :payload, :type
+
+    def common_fields
+      @_common_fields ||= CommonFieldsPresenter.new(payload)
+    end
+
+    def expanded_links
+      @_expanded_links ||= ExpandedLinksPresenter.new(payload["expanded_links"])
+    end
+
+    def indexable_content
+      @_indexable_content ||= IndexableContentPresenter.new(payload["details"])
+    end
   end
 end
