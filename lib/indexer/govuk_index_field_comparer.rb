97cb70218aa9a966972116938162fceef4505b08
David Henry <david@decoybecoy.com>
2017-10-04 14:17:59 +0100

    Start work to enable aaib_reports for indexing

--- lib/indexer/govuk_index_field_comparer.rb
+++ lib/indexer/govuk_index_field_comparer.rb
@@ -8,6 +8,9 @@ module Indexer
     def call(key, old, new)
       return compare_time(old, new) if key == 'public_timestamp'
       return compare_content(old, new) if key == 'indexable_content'
+      return true if %w(content_id publishing_app rendering_app content_store_document_type).include?(key) && old.nil?
+      return true if old.nil? && new == ''
+      return true if key == 'rendering_app' && old == 'specialist-frontend' && new == 'government-frontend'
       old == new
     end
 
@@ -18,42 +21,47 @@ module Indexer
     end
 
     def compare_content(old, new)
-      clean_old = clean_content(old)
+      clean_old = clean_content(remove_links(old))
       clean_new = clean_content(new)
       if clean_old == clean_new
         true
       else
-        old_words = clean_old.split(' ')
-        new_words = clean_new.split(' ')
+        old_words = clean_old.split(' ').compact
+        new_words = clean_new.split(' ').compact
 
         extra_old = old_words - new_words
-        diff = extra_old + (new_words - old_words)
-        diff_per = (200.0 * diff.count / (old_words.count + new_words.count))
+        extra_old.reject! { |w| new_words.any? { |new| new.include?(w) } } # ignore words that have been trimmed
+        extra_old.reject! { |w| w =~ /^\d+$/ } # ignore numbers
+
+        diff_per = (200.0 * extra_old.count / (old_words.count + new_words.count))
         if extra_old.count == 0
           # we don't need to worry too much about additional words being added
           return true
-        elsif clean_old =~ /https assets.digital.cabinet office.gov.uk/
-          # This is an issue with the link in the markdown, we should regex this out as a better solution
-          puts "link: #{old}"
-          return false
-        elsif diff_per < 5
+        elsif diff_per < 2
           # if the page has less than X % difference then we are just going to say it is close enough
           # This should be reviewed with the Product Manager
           return true
         else
           # These are the real difference as are printed to the screen so they can be review on an individual basis
-          puts "Mismatch content: #{diff_per.round(2)} : #{diff.length}\n`#{clean_old}`\n!=\n`#{clean_new}`\n\n"
+          puts "Mismatch content: #{diff_per.round(2)} : #{extra_old.length} : #{extra_old.join(', ')}\n`#{clean_old}`\n!=\n`#{clean_new}`\n\n"
           false
         end
       end
     end
 
+    def remove_links(str)
+      str.gsub(/\[([^\]]*)\]\([^\)]*\)/, ' \1 ')
+        .gsub(/#+\s*/, ' ')
+        .gsub(/\[InlineAttachment:([^\[\]]*(?:\[[^\]]*\][^\[\]]*|)*)\]/, '\1')
+    end
+
     def clean_content(str)
       (str || '')
-        .gsub(/\[InlineAttachment:([^\]]*)\.[^\.]*\]/) { $1.tr('_', ' ') } # remove inline attachment text
         .downcase # normalise case
-        .gsub(/\.[a-z]{3}( |$)/, '\1') # hash as sometimes the link will have the extension
-        .gsub(/[\s,\-_:\/]+/, ' ') # remove all special characters
+        .gsub(/\.[a-z]{3}(\)| |$)/, '\1') # hash as sometimes the link will have the extension
+        .gsub(/[\s,\-_:\/–\[\]\(\)\.\*]+/, ' ') # remove all special characters
+        .gsub(/&amp;/, '&')
+        .gsub(/[’'‘]/, "'")
     end
   end
 end
