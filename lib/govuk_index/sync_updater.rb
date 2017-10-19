10685e07594077f19879982981e28bd607296103
David Henry <david@decoybecoy.com>
2017-10-19 15:28:35 +0100

    Add functionality to run sync job immediately
    This means the sync can be run without using the sidekiq
    queue which is useful for testing

--- lib/govuk_index/sync_updater.rb
+++ lib/govuk_index/sync_updater.rb
@@ -7,19 +7,36 @@ module GovukIndex
       ).run
     end
 
+    def self.update_immediately(source_index: 'mainstream', destination_index: 'govuk', format_override:)
+      new(
+        source_index: source_index,
+        destination_index: destination_index,
+        format_override: format_override,
+      ).run(async: false)
+    end
+
     def self.worker
       SyncWorker
+    end
+
+    def initialize(source_index:, destination_index:, format_override: nil)
+      super(
+        source_index: source_index,
+        destination_index: destination_index,
+      )
+      @format_override = format_override
     end
 
   private
 
     def search_body
+      cause = @format_override ? :must : :must_not
       {
         query: {
           bool: {
-            must_not: {
+            cause => {
               terms: {
-                format: MigratedFormats.indexable_formats
+                format: Array(@format_override || MigratedFormats.indexable_formats)
               }
             }
           }
