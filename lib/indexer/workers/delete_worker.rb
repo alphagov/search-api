901d0b15571e411a26c7b63c2c51d59501776d4d
Mat Moore <mat.moore@digital.cabinet-office.gov.uk>
2017-11-16 11:01:37 +0000

    Log the type of anything deleted
    Some best bets aren't being deleted properly and I expect the type/id
    isn't being passed correctly.
    
    Also remove some temporary code from an old deploy.

--- lib/indexer/workers/delete_worker.rb
+++ lib/indexer/workers/delete_worker.rb
@@ -2,14 +2,9 @@ module Indexer
   class DeleteWorker < BaseWorker
     notify_of_failures
 
-    def perform(index_name, elasticsearch_type, document_id = nil)
-      # Handle previous method signature to cope with leftover queued jobs when
-      # we deploy.
-      if document_id.nil?
-        elasticsearch_type, document_id = index(index_name).link_to_type_and_id(elasticsearch_type)
-      end
+    def perform(index_name, elasticsearch_type, document_id)
+      logger.info "Deleting #{elasticsearch_type} document '#{document_id}' of type '#{elasticsearch_type}' from '#{index_name}'"
 
-      logger.info "Deleting #{elasticsearch_type} document '#{document_id}' from '#{index_name}'"
       begin
         index(index_name).delete(elasticsearch_type, document_id)
       rescue SearchIndices::IndexLocked
