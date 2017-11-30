586a52b9983bad17fd7693432d9555d5ef0e547d
Mat Moore <mat.moore@digital.cabinet-office.gov.uk>
2017-11-30 15:14:33 +0000

    Consistently use *.synonym fields and analyzers
    - Make the B version respect the debug parameter for disabling synonyms
    - Make query_analyzer take into account the A/B test, so the new approach is used consistently
    - Move all the logic for switching between different analyzers to `core_query`

--- lib/search/query_components/core_query.rb
+++ lib/search/query_components/core_query.rb
@@ -5,6 +5,11 @@ module QueryComponents
     DEFAULT_QUERY_ANALYZER = "query_with_old_synonyms".freeze
     DEFAULT_QUERY_ANALYZER_WITHOUT_SYNONYMS = "default".freeze
 
+    # Used with foo.synonym fields. Passing this is not necessary because
+    # it's the default for these fields. We're only passing it at query time
+    # to make the various possible queries more consistant with each other.
+    QUERY_TIME_SYNONYMS_ANALYZER = "with_search_synonyms".freeze
+
     # If the search query is a single quoted phrase, we run a different query,
     # which uses phrase matching across various fields.
     # Boost title the most, but ensure that organisations rank brilliantly
@@ -70,21 +75,10 @@ module QueryComponents
       ]
     end
 
-    def minimum_should_match_with_synonyms
-      {
-        match: {
-          "all_searchable_text.synonym" => {
-            query: escape(search_term),
-            minimum_should_match: MINIMUM_SHOULD_MATCH,
-          }
-        }
-      }
-    end
-
     def minimum_should_match(field_name)
       {
         match: {
-          field_name => {
+          synonym_field(field_name) => {
             query: escape(search_term),
             analyzer: query_analyzer,
             minimum_should_match: MINIMUM_SHOULD_MATCH,
@@ -96,7 +90,7 @@ module QueryComponents
     def match_phrase(field_name)
       {
         match_phrase: {
-          field_name => {
+          synonym_field(field_name) => {
             query: escape(search_term),
             analyzer: query_analyzer,
           }
@@ -105,6 +99,8 @@ module QueryComponents
     end
 
     def match_all_terms(fields)
+      fields = fields.map { |f| synonym_field(f) }
+
       {
         multi_match: {
           query: escape(search_term),
@@ -116,6 +112,8 @@ module QueryComponents
     end
 
     def match_bigrams(fields)
+      fields = fields.map { |f| synonym_field(f) }
+
       {
         multi_match: {
           query: escape(search_term),
@@ -127,21 +125,38 @@ module QueryComponents
     end
 
     def match_any_terms(fields)
+      fields = fields.map { |f| synonym_field(f) }
+
       {
         multi_match: {
           query: escape(search_term),
           operator: "or",
           fields: fields,
+          analyzer: query_analyzer
         }
       }
+    end
+
+    # Use the synonym variant of the field unless we're disabling synonyms
+    def synonym_field(field_name)
+      return field_name if search_params.disable_synonyms?
+      return field_name if !search_params.synonym_b_variant?
+      raise ValueError if field_name.include?(".")
+
+      field_name + ".synonym"
     end
 
   private
 
     def query_analyzer
       if search_params.disable_synonyms?
+        # this is the default defined in the mapping for regular fields
         DEFAULT_QUERY_ANALYZER_WITHOUT_SYNONYMS
+      elsif search_params.synonym_b_variant?
+        # this is the default defined in the mapping for *.synonym fields
+        QUERY_TIME_SYNONYMS_ANALYZER
       else
+        # this excludes the synonym filter
         DEFAULT_QUERY_ANALYZER
       end
     end
