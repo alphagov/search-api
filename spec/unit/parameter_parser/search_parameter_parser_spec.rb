63fa83b61eed45bddf60b37edaf465c08cf44ac5
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2018-06-28 13:08:18 +0100

    Appease the linting god

--- spec/unit/parameter_parser/search_parameter_parser_spec.rb
+++ spec/unit/parameter_parser/search_parameter_parser_spec.rb
@@ -179,7 +179,7 @@ RSpec.describe SearchParameterParser do
   it "complains about an overly large count parameter" do
     p = described_class.new({ "count" => %w(1501) }, @schema)
 
-    expect(%{Maximum result set size (as specified in 'count') is 1500}).to eq(p.error)
+    expect(p.error).to eq(%{Maximum result set size (as specified in 'count') is 1500})
     expect(p).not_to be_valid
     expect(expected_params(count: 10)).to eq(p.parsed_params)
   end
