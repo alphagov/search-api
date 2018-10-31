52bb0536f4f6b1b025c86294683f5fb518d48ceb
Jos Koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2018-10-31 15:10:27 +0000

    Simplify filtering queries
    Selected and rejected items are part of a 'must' and 'must_not' query
    respectively and are 'and'-ed by default and do not need additional wrapping

--- lib/search/query_components/filter.rb
+++ lib/search/query_components/filter.rb
@@ -6,40 +6,14 @@ module QueryComponents
       user_filter = QueryComponents::UserFilter.new(search_params)
       visibility_filter = QueryComponents::VisibilityFilter.new(search_params)
 
-      user_rejected = combine_filters(
-        user_filter.rejected_queries(excluded_field_names),
-        :and
-      )
-      visibility_rejected = combine_filters(
-        visibility_filter.rejected_queries,
-        :and
-      )
-      all_rejected_queries = [user_rejected, visibility_rejected]
+      rejected = user_filter.rejected_queries(excluded_field_names) + visibility_filter.rejected_queries
 
-      # *all* filters must be true to *include* in the result
-      selected = combine_filters(user_filter.selected_queries(excluded_field_names), :and)
+      selected = user_filter.selected_queries(excluded_field_names)
 
-      # *any* rejects can be true to *exclude* from the result
-      rejected = combine_filters(all_rejected_queries, :or)
-
-      if selected
-        if rejected
-          {
-            bool: {
-              must: selected,
-              must_not: rejected,
-            }
-          }
-        else
-          selected
-        end
-      elsif rejected
-        {
-          bool: {
-            must_not: rejected,
-          }
-        }
-      end
+      result = {}
+      result[:must] = selected unless selected.empty?
+      result[:must_not] = rejected unless rejected.empty?
+      result.empty? ? {} : { bool: result }
     end
   end
 end
