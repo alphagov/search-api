6c93e5e31f435739ee1437e7a98d22d7c633cc47
Steve Laing <steve.laing@gmail.com>
2018-11-22 10:14:39 +0000

    Lookup facet config from finder definition
    Facet config is stored in YAML in this project and should be
    treated as the single definition of the facets and values we
    tag with. This should reduce the impact of changes to metadata
    values and names.

--- spec/unit/indexer/metadata_tagger_spec.rb
+++ spec/unit/indexer/metadata_tagger_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe Indexer::MetadataTagger do
 
     metadata = {
       "sector_business_area" => %w(aerospace agriculture),
-      "employ_eu_citizens" => %w(yes),
+      "doing_business_in_the_eu" => %w(yes),
       "appear_in_find_eu_exit_guidance_business_finder" => "yes"
     }
 
@@ -43,10 +43,10 @@ RSpec.describe Indexer::MetadataTagger do
     metadata = {
       "sector_business_area" => ["accommodation-restaurants-and-catering-services", "aerospace", "agriculture", "air-transport-aviation", "ancillary-services", "animal-health", "automotive", "banking-market-infrastructure", "broadcasting", "chemicals", "computer-services", "construction-contracting", "education", "electricity", "electronics", "environmental-services", "fisheries", "food-and-drink", "furniture-and-other-manufacturing", "gas-markets", "goods-sectors-each-0-4-of-gva", "imports", "imputed-rent", "insurance", "land-transport-excl-rail", "medical-services", "motor-trades", "network-industries-0-3-of-gva", "oil-and-gas-production", "other-personal-services", "parts-and-machinery", "pharmaceuticals", "post", "professional-and-business-services", "public-administration-and-defence", "rail", "real-estate-excl-imputed-rent", "retail", "service-sectors-each-1-of-gva", "social-work", "steel-and-other-metals-commodities", "telecoms", "textiles-and-clothing", "top-ten-trade-partners-by-value", "warehousing-and-support-for-transportation", "water-transport-maritime-ports", "wholesale-excl-motor-vehicles"],
       "employ_eu_citizens" => ["yes", "no", "dont-know"],
-      "doing_business_in_the_eu" => ["do-business-in-the-eu", "buying", "selling", "transporting", "other-eu", "other-rest-of-the-world"],
-      "regulations_and_standards" => ["products-or-goods"],
+      "doing_business_in_the_eu" => ["do-business-in-the-eu", "buying", "selling", "transporting", "other-eu"],
+      "regulations_and_standards" => ["yes", "no"],
       "personal_data" => ["processing-personal-data", "interacting-with-eea-website", "digital-service-provider"],
-      "intellectual_property" => ["have-intellectual-property", "copyright", "trademarks", "designs", "patents", "exhaustion-of-rights"],
+      "intellectual_property" => ["copyright", "trademarks", "designs", "patents", "exhaustion-of-rights"],
       "receiving_eu_funding" => ["horizon-2020", "cosme", "european-investment-bank-eib", "european-structural-fund-esf", "eurdf", "etcf", "esc", "ecp", "etf"],
       "public_sector_procurement" => ["civil-government-contracts", "defence-contracts"],
       "appear_in_find_eu_exit_guidance_business_finder" => "yes"
