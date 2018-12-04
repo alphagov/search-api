6eb4ce5459d5d30bcef493f807396be48c1201a3
Bevan Loon <bevan.loon@digital.cabinet-office.gov.uk>
2018-12-04 09:20:11 +0000

    Add metadata destroyer
    Add function to set all metadata for the base paths in the associated
    CSV to nil. This should mean the items are no longer returned when
    the finder searches for them.

--- spec/unit/indexer/metadata_tagger_spec.rb
+++ spec/unit/indexer/metadata_tagger_spec.rb
@@ -27,5 +27,40 @@ RSpec.describe Indexer::MetadataTagger do
     described_class.initialise(fixture_file, facet_config_file)
     described_class.amend_all_metadata
   end
+
+  context "when removing metadata" do
+    def nil_metadata_hash
+      {
+        "business_activity" => nil,
+        "employ_eu_citizens" => nil,
+        "eu_uk_government_funding" => nil,
+        "regulations_and_standards" => nil,
+        "personal_data" => nil,
+        "intellectual_property" => nil,
+        "public_sector_procurement" => nil,
+        "sector_business_area" => nil,
+        "appear_in_find_eu_exit_guidance_business_finder" => nil
+      }
+    end
+
+    it "nils out all metadatad for a base path" do
+      fixture_file = File.expand_path("fixtures/metadata.csv", __dir__)
+      base_path = "/a_base_path"
+      test_index = "test_index"
+
+      mock_index = double("index")
+
+      expect_any_instance_of(LegacyClient::IndexForSearch).to receive(:get_document_by_link)
+        .and_return("real_index_name" => test_index)
+
+      expect(mock_index).to receive(:amend).with(base_path, nil_metadata_hash)
+      expect_any_instance_of(SearchIndices::SearchServer).to receive(:index)
+        .with(test_index)
+        .and_return(mock_index)
+
+      described_class.initialise(fixture_file, facet_config_file)
+      described_class.remove_all_metadata_for_base_paths(base_path)
+    end
+  end
   # rubocop:enable RSpec/VerifiedDoubles, RSpec/AnyInstance, RSpec/MessageSpies
 end
