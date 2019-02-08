15b528d1b4ff948856a15b4295084c00e262c119
Jos Koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2019-02-08 15:29:05 +0000

    Update popularity_worker_spec to satisfy linter

--- spec/unit/govuk_index/popularity_worker_spec.rb
+++ spec/unit/govuk_index/popularity_worker_spec.rb
@@ -1,42 +1,46 @@
 require 'spec_helper'
 
 RSpec.describe GovukIndex::PopularityWorker do
-  it "should save all records" do
+  subject(:worker) { described_class.new }
+
+  before do
+    allow(GovukDocumentTypes).to receive(:supertypes).
+      with(document_type: 'testgroup').
+      and_return("supertype1" => "type1", "supertype2" => "type2")
+    @processor = instance_double('processor', save: nil, commit: nil)
+    allow(Index::ElasticsearchProcessor).to receive(:new).and_return(@processor)
+  end
+
+  it "saves all records" do
     stub_popularity_data
-    processor = double(:processor)
-    allow(Index::ElasticsearchProcessor).to receive(:new).and_return(processor)
     records = [
       { 'identifier' => { '_id' => 'record_1' }, 'document' => {} },
       { 'identifier' => { '_id' => 'record_2' }, 'document' => {} },
     ]
+    worker.perform(records, "govuk_test")
 
-    expect(processor).to receive(:save).twice
-    expect(processor).to receive(:commit)
-
-    subject.perform(records, "govuk_test")
+    expect(@processor).to have_received(:save).twice
+    expect(@processor).to have_received(:commit)
   end
 
-  it "should update popularity field" do
+  it "updates popularity field" do
     stub_popularity_data('record_1' => 0.7)
 
-    processor = double(:processor)
-    allow(Index::ElasticsearchProcessor).to receive(:new).and_return(processor)
-    record = { 'identifier' => { '_id' => 'record_1' }, 'document' => { 'title' => 'test_doc' } }
+    @record = { 'identifier' => { '_id' => 'record_1' },
+                'document' => { 'title' => 'test_doc' } }
 
-    expect(processor).to receive(:save).with(
-      OpenStruct.new(
+    worker.perform([@record], "govuk_test")
+
+    expect(@processor).to have_received(:save).with(
+      having_attributes(
         identifier: { '_id' => 'record_1', '_version_type' => 'external_gte' },
-        document: { 'popularity' => 0.7, 'title' => 'test_doc' }
+        document: hash_including({ 'popularity' => 0.7, 'title' => 'test_doc' })
       )
     )
-    expect(processor).to receive(:commit)
-
-    subject.perform([record], "govuk_test")
   end
 
   def stub_popularity_data(data = Hash.new(0.5))
-    popularity_lookup = double(:popularity_lookup)
+    popularity_lookup = instance_double('popularity_lookup', lookup_popularities: data)
     allow(Indexer::PopularityLookup).to receive(:new).and_return(popularity_lookup)
-    allow(popularity_lookup).to receive(:lookup_popularities).and_return(data)
   end
 end
