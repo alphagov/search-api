6f0f2485f6d571775030940c7f5ab3235506196b
Jos Koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2019-03-06 12:10:51 +0000

    Fix bug in patching links in FinderPresenter
    The publish_document_finder rake task publishes a given content item
    and then performs a patch links api call to update the links in
    the content item.
    
    Removing links from an existing content item requires a call to
    patch links containing the relevant key and an empty array. Omitting
    the key will leave the link in place.
    
    This fix ensures that removing the email_alert_signup or parent links
    works properly.

--- lib/content_item_publisher/finder_presenter.rb
+++ lib/content_item_publisher/finder_presenter.rb
@@ -1,25 +1,15 @@
 module ContentItemPublisher
   class FinderPresenter < ContentItemPresenter
     def present_links
-      links = {}
-      links["email_alert_signup"] = email_alert_signup if email_alert_signup?
-      links["parent"] = content_item_parent if content_item_parent?
+      links = { "email_alert_signup" => email_alert_signup,
+               "parent" => content_item_parent }
 
       { content_id: content_id, links: links }
     end
 
   private
-
-    def email_alert_signup?
-      content_item.key?("signup_content_id")
-    end
-
     def email_alert_signup
-      [content_item["signup_content_id"]]
-    end
-
-    def content_item_parent?
-      content_item.key?("parent")
+      Array(content_item["signup_content_id"])
     end
 
     def content_item_parent
