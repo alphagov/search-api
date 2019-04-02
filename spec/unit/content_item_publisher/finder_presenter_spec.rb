042550f7795eabcc664b9b0627594b726ffb6dc3
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2019-04-02 08:54:17 +0100

    Make tests run against each yaml definition
    `let` statements are only evaluated once, so although these specs were running
    once for each file, the subject was the content of the first file loaded.
    
    We also fix the tests for email signup and parent links which now fail against
    some of the (valid) finder definitions

--- spec/unit/content_item_publisher/finder_presenter_spec.rb
+++ spec/unit/content_item_publisher/finder_presenter_spec.rb
@@ -4,43 +4,49 @@ require "govuk_schemas/rspec_matchers"
 RSpec.describe ContentItemPublisher::FinderPresenter do
   include GovukSchemas::RSpecMatchers
 
+  before do
+    GovukContentSchemaTestHelpers.configure do |config|
+      config.schema_type = 'publisher_v2'
+      config.project_root = File.expand_path(Dir.pwd)
+    end
+  end
+
   finders_glob = File.join(Dir.pwd, "config", "finders", "*_finder.yml")
+
   Dir.glob(finders_glob).each do |config_file|
+    context "Checking #{File.basename(config_file)}" do
+      subject(:instance) { described_class.new(finder, timestamp) }
 
-    subject(:instance) { described_class.new(finder, timestamp) }
+      let(:finder) { YAML.load_file(config_file) }
+      let(:content_id) { finder['content_id'] }
+      let(:timestamp) { Time.now.iso8601 }
 
-    let(:finder) { YAML.load_file(config_file) }
-    let(:content_id) { finder["content_id"] }
-    let(:timestamp) { Time.now.iso8601 }
-
-    before do
-      GovukContentSchemaTestHelpers.configure do |config|
-        config.schema_type = 'publisher_v2'
-        config.project_root = File.expand_path(Dir.pwd)
+      it "presents a valid payload" do
+        expect(instance.present).to be_valid_against_schema("finder")
       end
-    end
 
-    it "presents a valid payload" do
-      expect(instance.present).to be_valid_against_schema("finder")
-    end
+      it "exposes the content_id" do
+        expect(instance.content_id).to eq(content_id)
+      end
 
-    it "exposes the content_id" do
-      expect(instance.content_id).to eq(content_id)
-    end
+      it "sets the public_updated_at value" do
+        expect(instance.present[:public_updated_at]).to eq(timestamp)
+      end
 
-    it "sets the public_updated_at value" do
-      expect(instance.present[:public_updated_at]).to eq(timestamp)
-    end
+      it "sets the links hash" do
+        email_signup_links = [finder['signup_content_id']].compact
+        parent_links = [finder['parent']].compact
+        expect(instance.present_links[:links]).to eq({ "email_alert_signup" => email_signup_links,
+                                                      "parent" => parent_links })
+      end
 
-    it "sets the links hash" do
-      expect(instance.present_links[:links]).to eq({ "email_alert_signup" => [finder['signup_content_id']],
-                                                     "parent" => [] })
-    end
+      it "uses empty arrays to remove links" do
+        finder_with_no_links = finder.except("parent").except("signup_content_id")
+        presenter_with_empty_links = described_class.new(finder_with_no_links, timestamp)
 
-    it "uses empty arrays to remove links" do
-      finder.except!("signup_content_id")
-      expect(instance.present_links[:links]).to eq({ "email_alert_signup" => [],
-                                                     "parent" => [] })
+        expect(presenter_with_empty_links.present_links[:links]).to eq({ "email_alert_signup" => [],
+                                                      "parent" => [] })
+      end
     end
   end
 end
