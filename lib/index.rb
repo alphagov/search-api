1d21bd40ad05e9e5ea5351f9b85180bb95e67abc
Michael Walker <mike@barrucadu.co.uk>
2019-07-31 14:13:25 +0100

    Handle best bet analyzer returning nothing
    With ES5 if the best bet analyzer finds no tokens, we get this
    response:
    
        {
           "tokens" : []
        }
    
    However, with ES6 we get this response:
    
        {
        }
    
    No tokens!

--- lib/index.rb
+++ lib/index.rb
@@ -192,7 +192,7 @@ module SearchIndices
           },
         )
 
-        analyzed_query["tokens"].map { |token_info|
+        analyzed_query.fetch('tokens', []).map { |token_info|
           token_info["token"]
         }.join(" ")
       rescue Elasticsearch::Transport::Transport::Errors::BadRequest
