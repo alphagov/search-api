7c3d07c9bed0787a188e99c997b351a9e9d455f5
Michael Walker <mike@barrucadu.co.uk>
2019-08-01 15:27:51 +0100

    When given a sort order, break ties by sorting on content ID

--- lib/search/query_components/sort.rb
+++ lib/search/query_components/sort.rb
@@ -10,7 +10,9 @@ module QueryComponents
         # Sort by popularity when there's no explicit ordering, and there's no
         # query (so there's no relevance scores).
         elsif search_term.nil? && !search_params.disable_popularity?
-          return [{ "popularity" => { order: "desc" } }]
+          return [{ "popularity" => { order: "desc" } }, tie_breaking_sort]
+        # If there's no explicit ordering, but there is a query (and
+        # so there are relevance scores), sort by relevance
         else
           return nil
         end
@@ -28,8 +30,17 @@ module QueryComponents
             # really) with a missing value.
             unmapped_type: "integer"
           }
-       }
+        },
+        tie_breaking_sort,
       ]
+    end
+
+    # use content ID (which never changes) as a tie breaker if two
+    # items are otherwise equally placed in the results -- this is so
+    # we're explicit about how to break the tie, rather than relying
+    # on however Elasticsearch chooses to do it.
+    def tie_breaking_sort
+      { "content_id" => { order: "asc" } }
     end
   end
 end
