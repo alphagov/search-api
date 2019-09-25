855c493b153967842e50c6bafe41f41d35421bef
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2019-09-25 12:02:28 +0100

    Fix RSpec/EmptyLineAfterHook offences

--- spec/unit/search/format_migrator_spec.rb
+++ spec/unit/search/format_migrator_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe Search::FormatMigrator do
     allow_any_instance_of(LegacyClient::IndexForSearch).to receive(:real_index_names).and_return(%w(govuk_test))
   end
   # rubocop:enable RSpec/AnyInstance
+
   context "with every cluster" do
     Clusters.active.each do |_cluster|
       it "when base query without migrated formats" do
