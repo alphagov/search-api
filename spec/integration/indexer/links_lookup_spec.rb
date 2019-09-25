cb00dd69390574272d240980ece5409bb1f4ef10
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2019-09-25 12:19:09 +0100

    Fix Naming/VariableNumber offences

--- spec/integration/indexer/links_lookup_spec.rb
+++ spec/integration/indexer/links_lookup_spec.rb
@@ -140,59 +140,59 @@ RSpec.describe "TaglookupDuringIndexingTest" do
   end
 
   it "extracts parts of taxonomy" do
-    grandparent_1_content_id = "22aadc14-9bca-40d9-abb4-4f21f9792a05"
-    grandparent_1 = {
-      "content_id" => grandparent_1_content_id,
+    grandparent1_content_id = "22aadc14-9bca-40d9-abb4-4f21f9792a05"
+    grandparent1 = {
+      "content_id" => grandparent1_content_id,
       "base_path" => "/grandparent-1",
       "title" => "Grandparent 1",
       "links" => {},
     }
 
-    parent_1_content_id = "11aadc14-9bca-40d9-abb4-4f21f9792a05"
-    parent_1 = {
-      "content_id" => parent_1_content_id,
+    parent1_content_id = "11aadc14-9bca-40d9-abb4-4f21f9792a05"
+    parent1 = {
+      "content_id" => parent1_content_id,
       "base_path" => "/parent-1",
       "title" => "Parent 1",
       "links" => {
-        "parent_taxons" => [grandparent_1],
+        "parent_taxons" => [grandparent1],
       },
     }
 
-    taxon_1_content_id = "00aadc14-9bca-40d9-abb4-4f21f9792a05"
-    taxon_1 = {
-      "content_id" => taxon_1_content_id,
+    taxon1_content_id = "00aadc14-9bca-40d9-abb4-4f21f9792a05"
+    taxon1 = {
+      "content_id" => taxon1_content_id,
       "base_path" => "/this-is-a-taxon",
       "title" => "Taxon 1",
       "links" => {
-        "parent_taxons" => [parent_1],
+        "parent_taxons" => [parent1],
       },
     }
 
-    grandparent_2_content_id = "03aadc14-9bca-40d9-abb4-4f21f9792a05"
-    grandparent_2 = {
-      "content_id" => grandparent_2_content_id,
+    grandparent2_content_id = "03aadc14-9bca-40d9-abb4-4f21f9792a05"
+    grandparent2 = {
+      "content_id" => grandparent2_content_id,
       "base_path" => "/grandparent-2",
       "title" => "Grandparent 2",
       "links" => {},
     }
 
-    parent_2_content_id = "02aadc14-9bca-40d9-abb4-4f21f9792a05"
-    parent_2 = {
-      "content_id" => parent_2_content_id,
+    parent2_content_id = "02aadc14-9bca-40d9-abb4-4f21f9792a05"
+    parent2 = {
+      "content_id" => parent2_content_id,
       "base_path" => "/parent-2",
       "title" => "Parent 2",
       "links" => {
-        "parent_taxons" => [grandparent_2],
+        "parent_taxons" => [grandparent2],
       },
     }
 
-    taxon_2_content_id = "01aadc14-9bca-40d9-abb4-4f21f9792a05"
-    taxon_2 = {
-      "content_id" => taxon_2_content_id,
+    taxon2_content_id = "01aadc14-9bca-40d9-abb4-4f21f9792a05"
+    taxon2 = {
+      "content_id" => taxon2_content_id,
       "base_path" => "/this-is-also-a-taxon",
       "title" => "Taxon 2",
       "links" => {
-        "parent_taxons" => [parent_2],
+        "parent_taxons" => [parent2],
       },
     }
 
@@ -203,7 +203,7 @@ RSpec.describe "TaglookupDuringIndexingTest" do
     publishing_api_has_expanded_links(
       content_id: "DOCUMENT-CONTENT-ID",
       expanded_links: {
-        taxons: [taxon_1, taxon_2],
+        taxons: [taxon1, taxon2],
       },
     )
 
@@ -215,10 +215,10 @@ RSpec.describe "TaglookupDuringIndexingTest" do
       {
         "link" => "/foo/bar",
         "part_of_taxonomy_tree" => [
-          grandparent_1_content_id, parent_1_content_id, taxon_1_content_id,
-          grandparent_2_content_id, parent_2_content_id, taxon_2_content_id
+          grandparent1_content_id, parent1_content_id, taxon1_content_id,
+          grandparent2_content_id, parent2_content_id, taxon2_content_id
         ],
-        "taxons" => [taxon_1_content_id, taxon_2_content_id],
+        "taxons" => [taxon1_content_id, taxon2_content_id],
       },
       index: "government_test",
     )
