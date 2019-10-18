df6ee4fb16ff5f66ac71abb3e59a284959ffca82
Laurent Curau <hello@laurentcurau.com>
2019-10-18 12:45:55 +0100

    Use correct aws-sdk syntax

--- lib/sitemap/uploader.rb
+++ lib/sitemap/uploader.rb
@@ -5,9 +5,8 @@ module Sitemap
     end
 
     def upload(file_content:, file_name:)
-      puts "Uploading #{file_name} ..."
       o = Aws::S3::Object.new(bucket_name: @bucket_name, key: file_name)
-      raise "Failed to create sitemap file '#{file_name}'" unless o.put(file_content)
+      raise "Failed to create sitemap file '#{file_name}'" unless o.put(body: file_content)
     end
   end
 end
