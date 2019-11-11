e5c4846f9d1f69f438178995255705c03bc84466
Bill Franklin <william.franklin@digital.cabinet-office.gov.uk>
2019-11-11 11:24:58 +0000

    Add tests for LearnToRank::Reranker
    Also adds RankerTestHelpers

---
+++ spec/unit/helpers/ranker_test_helpers.rb
@@ -0,0 +1,45 @@
+require "spec_helper"
+
+module RankerTestHelpers
+  include Fixtures::LearnToRankExplain
+
+  def search_results
+    [
+      {
+        "_explanation" => default_explanation,
+        "_score" => 0.98,
+        "_source" => {
+          "popularity" => 10,
+          "title" => "More popular document",
+        },
+      },
+      {
+        "_explanation" => default_explanation,
+        "_score" => 0.97,
+        "_source" => {
+          "popularity" => 5,
+          "title" => "More relevant document",
+        },
+      },
+    ]
+  end
+
+  def stub_request_to_ranker(examples, rank_response)
+    stub_request(:post, "http://reranker:8501/v1/models/ltr:regress")
+      .with(
+        body: {
+          signature_name: "regression",
+          examples: examples,
+        }.to_json,
+        headers: {
+          "Content-Type" => "application/json",
+        },
+      )
+      .to_return(status: 200, body: { "results" => rank_response }.to_json)
+  end
+
+  def stub_ranker_is_unavailable
+    stub_request(:post, "http://reranker:8501/v1/models/ltr:regress")
+      .to_return(status: 500)
+  end
+end
--- spec/unit/learn_to_rank/reranker_spec.rb
+++ spec/unit/learn_to_rank/reranker_spec.rb
@@ -1,34 +1,38 @@
 require "spec_helper"
 require "spec/unit/helpers/ranker_test_helpers"
 
-RSpec.describe LearnToRank::Ranker do
+RSpec.describe LearnToRank::Reranker do
   include RankerTestHelpers
 
-  let(:ranks) { described_class.new(feature_sets).ranks }
+  let(:reranked) { described_class.new.rerank(es_results: search_results) }
 
   let(:feature_sets) do
     LearnToRank::FeatureSets.new.call(search_results)
   end
 
-  describe "#ranks" do
+  describe "#reranked" do
     context "when there are no search results" do
       let(:search_results) { [] }
       it "returns an empty array without calling ranker" do
-        expect(ranks).to eq([])
+        expect(reranked).to eq([])
       end
     end
 
     context "when there are search results" do
       it "returns an array of new ranks" do
         stub_request_to_ranker(feature_sets, [1, 2])
-        expect(ranks).to eq([1, 2])
+        expect(reranked.count).to eq(2)
+        expect(reranked.first.dig("_source", "title")).to eq("More relevant document")
+        expect(reranked.second.dig("_source", "title")).to eq("More popular document")
       end
     end
 
     context "when the ranker is unavailable" do
       it "returns an array of ranks in descending order, to preserve original rank" do
         stub_ranker_is_unavailable
-        expect(ranks).to eq([2, 1])
+        expect(reranked.count).to eq(2)
+        expect(reranked.first.dig("_source", "title")).to eq("More popular document")
+        expect(reranked.second.dig("_source", "title")).to eq("More relevant document")
       end
     end
   end
