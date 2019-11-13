88a12e20bc5f357b0ff87faeaae7ebb9bfc85c24
Bill Franklin <william.franklin@digital.cabinet-office.gov.uk>
2019-11-13 12:42:34 +0000

    Improve automated relevancy judgement generation
    This makes an improvement to how we generate training
    data from Google Analytics click data.
    
    Rather than using heavily sampled Google Analytics
    aggregate click through rate data for each query, which
    leads to inaccurate data for training on, this uses
    a CSV of BigQuery data and generates a training dataset
    from this.
    
    The training dataset computes the estimated CTR at position 1
    'CTR@1' for each query:document pair. CTR@1 is the percentage
    of clicks a link would get if we put it at the top of the results.
    
    The CTR@1 is a hack, to get around using a click model which
    might be more accurate.
    
    We turn the CTR@1 for each result into a score between 0-3,
    a relevancy score which is equivalent to the manual relevancy
    scores that we have collected previously.
    
    Finally the relevancy judgements have features embedded and
    are turned into LibSVM formatted data, for training the model.
    
    https://trello.com/c/VuvDR3Ac/1143-automatically-generate-relevancy-judgements
    
    Later we can automate the fetching of BigQuery data, to make
    it possible to run a single command to generate a model trained
    on the latest click data.

---
+++ lib/learn_to_rank/load_search_queries.rb
@@ -0,0 +1,21 @@
+require "csv"
+
+module LearnToRank
+  module LoadSearchQueries
+    def self.from_csv(datafile)
+      queries = {}
+      CSV.foreach(datafile, headers: true) do |row|
+        # Todo change the column names in the query
+        query = row["searchTerm"].strip
+        queries[query] ||= []
+        queries[query] << {
+          content_id: row["contentId"],
+          rank: row["avg_rank"],
+          views: row["views"],
+          clicks: row["clicks"],
+        }
+      end
+      queries
+    end
+  end
+end
--- ltr_scripts/train.sh
+++ ltr_scripts/train.sh
@@ -4,7 +4,7 @@ OUTPUT_DIR=${OUTPUT_DIR:-../tmp/libsvm}
 TRAIN=${TRAIN:-../tmp/ltr_data/train.txt}
 VALI=${VALI:-../tmp/ltr_data/validate.txt}
 TEST=${TEST:-../tmp/ltr_data/test.txt}
-STEPS=${STEPS:-100}
+STEPS=${STEPS:-5000}
 
 if [[ -d $OUTPUT_DIR ]]; then
   rm -rf $OUTPUT_DIR
