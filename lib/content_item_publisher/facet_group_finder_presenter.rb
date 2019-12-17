d1eed978cdc4ed7f2d3ca46296baef851c07d37a
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2019-12-17 14:35:22 +0000

    Remove redundant task to publish facet groups
    Previously we added a rake task specifically to publish facet values for
    the Business Finder. This removes it again, since the existing task to
    publish the Business Finder pages was changed to provide the behaviour
    of publishing the associated facet_groups.
    
    https://github.com/alphagov/search-api/commit/01322ac546fb262367e8481a7152087f8fb5b769#diff-4ef5ba4ffa61989fb2fcd58de5ea1a26

--- lib/content_item_publisher/facet_group_finder_presenter.rb
+++
@@ -1,9 +0,0 @@
-module ContentItemPublisher
-  class FacetGroupFinderPresenter < FinderPresenter
-    def present_links
-      links = default_links
-      links[:links]["facet_group"] = Array(content_item.fetch("links", {}).fetch("facet_group", nil))
-      links
-    end
-  end
-end
--- lib/content_item_publisher/facet_group_finder_publisher.rb
+++
@@ -1,9 +0,0 @@
-module ContentItemPublisher
-  class FacetGroupFinderPublisher < Publisher
-  private
-
-    def content_item_presenter
-      FacetGroupFinderPresenter
-    end
-  end
-end
