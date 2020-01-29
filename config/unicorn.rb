fd8d37cb76782da391d3e309e59d4eec10f07789
Bill Franklin <william.franklin@digital.cabinet-office.gov.uk>
2020-01-29 16:58:45 +0000

    Deployment stability improvements
    This makes a couple of changes to the unicorn config in
    an attempt to improve deployment stability.
    
    In order to increase application startup times, this adds
    https://github.com/Shopify/bootsnap
    
    Boot times are improved by using bootsnap, but initial requests
    to a newly booted worker are still slow;
    this change attempts to mitigate the impact of this.
    
    https://trello.com/c/CVrZaaPv/1320

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,3 +3,15 @@ require "govuk_app_config/govuk_unicorn"
 GovukUnicorn.configure(self)
 
 working_directory File.dirname(File.dirname(__FILE__))
+
+# Preload the entire app. By preloading an application you can save some
+# RAM resources as well as speed up server boot times.
+preload_app true
+
+before_fork do |_server, _worker|
+  # Throttles the master from starting new workers too quickly.
+  # Creates a more gradual roll out of workers so that users are
+  # less likely to have their request handled by a worker that has
+  # just booted, and may respond to requests slower.
+  sleep 5 unless ENV["RACK_ENV"] == "development"
+end
