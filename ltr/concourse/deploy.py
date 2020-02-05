f3f0ac05f97b669a5e522a945829d7163019b166
Michael Walker <mike@barrucadu.co.uk>
2020-02-05 12:30:49 +0000

    Have Concourse deploy script create endpoint if it doesn't exist
    + remove bootstrap jobs
    
    This makes it easier to deploy multiple models, as we can just deploy
    each to a different endpoint name.  If the endpoint doesn't exist
    it'll be created with some default parameters.

--- ltr/concourse/deploy.py
+++ ltr/concourse/deploy.py
@@ -13,19 +13,33 @@ role = os.environ["ROLE_ARN"]
 model_name = os.environ["SCRIPT_INPUT_DATA"].strip()
 
 s3_bucket = os.getenv("S3_BUCKET", f"govuk-{govuk_environment}-search-relevancy")
+default_initial_instance_count = int(os.getenv("INITIAL_INSTANCE_COUNT", 3))
+default_instance_type = os.getenv("INSTANCE_TYPE", "ml.t2.medium")
 endpoint_name = os.getenv(
     "ENDPOINT_NAME", f"govuk-{govuk_environment}-search-ltr-endpoint"
 )
 
 session = sagemaker.Session()
 
-# find the current endpoint config
-current_endpoint_config_name = session.boto_session.client(
-    "sagemaker"
-).describe_endpoint(EndpointName=endpoint_name)["EndpointConfigName"]
-current_endpoint_config = session.boto_session.client(
-    "sagemaker"
-).describe_endpoint_config(EndpointConfigName=current_endpoint_config_name,)
+initial_instance_count = default_initial_instance_count
+instance_type = default_instance_type
+update_endpoint = False
+
+# try tofind the current endpoint config
+try:
+    current_endpoint_config_name = session.boto_session.client(
+        "sagemaker"
+    ).describe_endpoint(EndpointName=endpoint_name)["EndpointConfigName"]
+    current_endpoint_config = session.boto_session.client(
+        "sagemaker"
+    ).describe_endpoint_config(EndpointConfigName=current_endpoint_config_name,)
+    initial_instance_count = current_endpoint_config["ProductionVariants"][0][
+        "InitialInstanceCount"
+    ]
+    instance_type = current_endpoint_config["ProductionVariants"][0]["InstanceType"]
+    update_endpoint = True
+except Exception:
+    print("could not find current endpoint, will create a new one", file=sys.stderr)
 
 # find the model
 model_keys = session.boto_session.client("s3").list_objects_v2(
@@ -35,21 +49,20 @@ if len(model_keys) != 1:
     raise Exception("Expected exactly 1 model key")
 model_key = model_keys[0]["Key"]
 
-# deploy the model by creating a new endpoint config and updating the existing endpoint
-model = sagemaker.tensorflow.serving.Model(
+# deploy the model by creating a new endpoint config and updating the
+# existing endpoint (or creating a new one)
+sagemaker.tensorflow.serving.Model(
     f"s3://{s3_bucket}/{model_key}", role, framework_version="2.0.0"
-)
-
-model.deploy(
-    current_endpoint_config["ProductionVariants"][0]["InitialInstanceCount"],
-    current_endpoint_config["ProductionVariants"][0]["InstanceType"],
+).deploy(
+    initial_instance_count,
+    instance_type,
     endpoint_name=endpoint_name,
-    update_endpoint=True,
+    update_endpoint=update_endpoint,
 )
 
 # wait for the deployment to complete
 endpoint_status = "Updating"
-while endpoint_status == "Updating":
+while endpoint_status in ["Creating", "Updating"]:
     print("Waiting for new model to be deployed...", file=sys.stderr)
     endpoint_status = session.boto_session.client("sagemaker").describe_endpoint(
         EndpointName=endpoint_name
