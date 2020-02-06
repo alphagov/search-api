149481080b091081be40eb2375652d4e44236469
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2020-02-06 17:26:56 +0000

    Remove business finder from special_route publisher

--- lib/special_route_publisher.rb
+++ lib/special_route_publisher.rb
@@ -100,14 +100,6 @@ class SpecialRoutePublisher
       },
       {
         rendering_app: "finder-frontend",
-        base_path: "/prepare-business-uk-leaving-eu",
-        content_id: "b9ef4434-761f-49ae-af97-dc7a248499c4",
-        title: "Prepare your business for the UK leaving the EU",
-        description: "Q&A frontend for the 'Find EU Exit guidance for your business' finder",
-        type: "exact",
-      },
-      {
-        rendering_app: "finder-frontend",
         base_path: "/uk-nationals-living-eu",
         content_id: "7a99da17-e9e1-410b-b67d-c3f6348c595d",
         title: "UK nationals living in the EU",
