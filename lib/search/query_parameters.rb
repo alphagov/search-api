bc9e05553b2b45776367e8703d54c3b65015e1b6
Bill Franklin <william.franklin@digital.cabinet-office.gov.uk>
2020-02-21 17:21:30 +0000

    Permit usage of Hippo and Elephant models

--- lib/search/query_parameters.rb
+++ lib/search/query_parameters.rb
@@ -8,7 +8,7 @@ module Search
     # starts and ends with quotes with no quotes in between, with or without
     # leading or trailing whitespace
     QUOTED_STRING_REGEX = /^\s*"[^"]+"\s*$/.freeze
-    MODEL_VARIANTS = %w().freeze
+    MODEL_VARIANTS = %w(hippo elephant).freeze
 
     def initialize(params = {})
       params = {
