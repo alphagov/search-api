abb8329194d40ea9221818e960dd933a4bd0fe5d
Michael Walker <mike@barrucadu.co.uk>
2020-03-31 15:41:04 +0100

    Don't try to turn attachments with missing URLs into parts
    Until the whitehall part of this PR is deployed, whitehall will
    continue to send attachments that have content but no URL.  So we need
    to be resilient to that.
    
    One option would be to just deploy the changes in quick succession and
    reindex any documents published in the gap, but being resilient to
    unexpected input is good practice anyway.

--- lib/indexer/parts_lookup.rb
+++ lib/indexer/parts_lookup.rb
@@ -12,7 +12,7 @@ module Indexer
     def prepare_parts(doc_hash, return_raw_body)
       return doc_hash unless doc_hash["parts"].nil?
 
-      attachments = doc_hash.fetch("attachments", []).select { |a| a.key? "content" }
+      attachments = doc_hash.fetch("attachments", []).select { |a| !a["url"].nil? && !a["content"].nil? }
       return doc_hash if attachments.empty?
 
       doc_hash.merge("parts" => attachments.map { |a| present_part(a, return_raw_body) })
