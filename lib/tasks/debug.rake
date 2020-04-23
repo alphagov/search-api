alphagov/search-api

commit 96ad601d728f0fa21031ed950535408d4179ffa9

Fix linting issue with top-level methods
We should avoid top-level methods, as they break code encapsulation.
This replaces the use of top-level methods in several rake tasks by
replacing the call to the method with the code of the method. While
this makes a couple of tasks more complicated, we can always refactor
these in future if they need maintenance.

lib/tasks/debug.rake
@@ -59,7 +59,16 @@ namespace :debug do
 
   desc "Check how well the search query performs for a set of relevancy judgements"
   task :ranking_evaluation, [:datafile, :ab_tests] do |_, args|
-    csv = args.datafile || relevancy_judgements_from_s3
+    csv = args.datafile || begin
+      bucket_name = ENV["AWS_S3_RELEVANCY_BUCKET_NAME"]
+      raise "Missing required AWS_S3_RELEVANCY_BUCKET_NAME envvar" if bucket_name.nil?
+
+      csv = Tempfile.open(["judgements", ".csv"])
+      o = Aws::S3::Object.new(bucket_name: bucket_name, key: filename)
+      o.get(response_target: csv.path)
+      csv.path
+    end
+
     begin
       evaluator = Debug::RankEval.new(csv, args.ab_tests)
       results = evaluator.evaluate
@@ -70,7 +79,13 @@ namespace :debug do
       end
       puts "---"
       puts "overall score: #{results[:score]}"
-      send_to_graphite("overall_score.rank_eval", results[:score])
+
+      if ENV["SEND_TO_GRAPHITE"]
+        Services.statsd_client.gauge(
+          "relevancy.query.overall_score.rank_eval",
+          results[:score],
+        )
+      end
     ensure
       if csv.is_a?(Tempfile)
         csv.close
@@ -91,24 +106,5 @@ namespace :debug do
         out << ["worst", link]
       end
     end
-  end
-
-  def relevancy_judgements_from_s3(filename: "judgements.csv")
-    bucket_name = ENV["AWS_S3_RELEVANCY_BUCKET_NAME"]
-    raise "Missing required AWS_S3_RELEVANCY_BUCKET_NAME envvar" if bucket_name.nil?
-
-    csv = Tempfile.open(["judgements", ".csv"])
-    o = Aws::S3::Object.new(bucket_name: bucket_name, key: filename)
-    o.get(response_target: csv.path)
-    csv.path
-  end
-
-  def send_to_graphite(query, score)
-    return unless ENV["SEND_TO_GRAPHITE"]
-
-    Services.statsd_client.gauge(
-      "relevancy.query.#{query.downcase.gsub(' ', '_')}",
-      score,
-    )
   end
 end

lib/tasks/export.rake
@@ -19,17 +19,19 @@ namespace :export do
         client: Services.elasticsearch(hosts: base_uri),
         index_names: SearchConfig.content_index_names + [SearchConfig.govuk_index_name],
         search_body: query,
-      ) { |hit| hit }.each { |hit| csv << fields.map { |f| present_field(hit["_source"][f]) } }
-    end
-  end
+      ) do |hit|
+        csv << fields.map do |f|
+          value = hit["_source"][f]
 
-  def present_field(value)
-    if value.is_a? Hash
-      value.fetch("slug", value)
-    elsif value.is_a? Array
-      value.join(",")
-    else
-      value
+          if value.is_a? Hash
+            value.fetch("slug", value)
+          elsif value.is_a? Array
+            value.join(",")
+          else
+            value
+          end
+        end
+      end
     end
   end
 end

lib/tasks/learn_to_rank.rake
@@ -10,23 +10,38 @@ require "relevancy/load_judgements"
 namespace :learn_to_rank do
   desc "Fetch data from BigQuery.  This costs money!"
   task :fetch_bigquery_export, [:credentials, :output] do |_, args|
-    assert_ltr!
+    raise 'set $ENABLE_LTR to "true" to use learn_to_rank' unless Search::RelevanceHelpers.ltr_enabled?
+
     data = LearnToRank::DataPipeline::Bigquery.fetch(JSON.parse(Base64.decode64(args.credentials)))
-    export_to_csv(data, args[:output])
+
+    CSV.open("tmp/#{args[:output]}.csv", "wb") do |csv|
+      csv << data.first.keys
+      data.each do |row|
+        csv << row.values
+      end
+    end
   end
 
   desc "Export a CSV of relevancy judgements generated from CTR on popular queries"
   task :generate_relevancy_judgements, [:queries, :output] do |_, args|
-    assert_ltr!
+    raise 'set $ENABLE_LTR to "true" to use learn_to_rank' unless Search::RelevanceHelpers.ltr_enabled?
+
     queries = LearnToRank::DataPipeline::LoadSearchQueries.from_csv(args.queries)
     generator = LearnToRank::DataPipeline::RelevancyJudgements.new(queries: queries)
     judgements = generator.relevancy_judgements
-    export_to_csv(judgements.force, args.output)
+    data = judgements.force
+
+    CSV.open("tmp/#{args.output}.csv", "wb") do |csv|
+      csv << data.first.keys
+      data.each do |row|
+        csv << row.values
+      end
+    end
   end
 
   desc "Export a CSV of SVM-formatted relevancy judgements for training a model"
   task :generate_training_dataset, [:judgements, :output_dir] do |_, args|
-    assert_ltr!
+    raise 'set $ENABLE_LTR to "true" to use learn_to_rank' unless Search::RelevanceHelpers.ltr_enabled?
 
     csv = args.judgements || "tmp/autogenerated_relevancy_judgements.csv"
     svm_dir = args.output_dir || "tmp/ltr_data"
@@ -53,7 +68,7 @@ namespace :learn_to_rank do
   namespace :reranker do
     desc "Train a reranker model with relevancy judgements"
     task :train, [:svm_dir, :model_dir] do |_, args|
-      assert_ltr!
+      raise 'set $ENABLE_LTR to "true" to use learn_to_rank' unless Search::RelevanceHelpers.ltr_enabled?
 
       model_dir = args.model_dir || "tmp/libsvm"
       svm_dir = args.svm_dir || "tmp/ltr_data"
@@ -62,7 +77,7 @@ namespace :learn_to_rank do
 
     desc "Serves a trained model"
     task :serve, [:model_dir] do |_, args|
-      assert_ltr!
+      raise 'set $ENABLE_LTR to "true" to use learn_to_rank' unless Search::RelevanceHelpers.ltr_enabled?
 
       model_dir = args.model_dir || "tmp/libsvm"
       sh "env EXPORT_PATH=#{__dir__}/../../#{model_dir} ./ltr/scripts/serve.sh"
@@ -70,7 +85,7 @@ namespace :learn_to_rank do
 
     desc "Evaluate search performance using nDCG with and without the model"
     task :evaluate, [:relevancy_judgements] do |_, args|
-      assert_ltr!
+      raise 'set $ENABLE_LTR to "true" to use learn_to_rank' unless Search::RelevanceHelpers.ltr_enabled?
 
       ndcg_at = "10"
 
@@ -112,19 +127,6 @@ namespace :learn_to_rank do
       puts "Without model: #{merged['average_ndcg'][:without]}"
       puts "With model: #{merged['average_ndcg'][:with_model]}"
       puts "The model has a #{winning ? 'good' : 'bad'} score"
-    end
-  end
-
-  def assert_ltr!
-    raise 'set $ENABLE_LTR to "true" to use learn_to_rank' unless Search::RelevanceHelpers.ltr_enabled?
-  end
-
-  def export_to_csv(hash, filename)
-    CSV.open("tmp/#{filename}.csv", "wb") do |csv|
-      csv << hash.first.keys
-      hash.each do |row|
-        csv << row.values
-      end
     end
   end
 end

commit fd93e86ee8522122649c48f6844bcf370102bd09

Fix errors introduced in 96ad601

lib/tasks/debug.rake
@@ -64,7 +64,7 @@ namespace :debug do
       raise "Missing required AWS_S3_RELEVANCY_BUCKET_NAME envvar" if bucket_name.nil?
 
       csv = Tempfile.open(["judgements", ".csv"])
-      o = Aws::S3::Object.new(bucket_name: bucket_name, key: filename)
+      o = Aws::S3::Object.new(bucket_name: bucket_name, key: "judgements.csv")
       o.get(response_target: csv.path)
       csv.path
     end

lib/tasks/relevancy.rake
@@ -101,6 +101,11 @@ def report(stats = [])
   puts "=================="
   stats.each do |(stat, reading)|
     puts "#{stat.downcase.gsub(' ', '_')}: #{reading}"
-    send_to_graphite(stat, reading)
+    if ENV["SEND_TO_GRAPHITE"]
+      Services.statsd_client.gauge(
+        "relevancy.query.#{stat.downcase.gsub(' ', '_')}",
+        results[:score],
+      )
+    end
   end
 end