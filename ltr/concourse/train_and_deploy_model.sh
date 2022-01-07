75156018447ad4e2fb2e54f23e94f3ffeb471120
Sean Rankine <sean.rankine@me.com>
2022-01-07 16:25:55 +0000

    removed escaped env var

--- ltr/concourse/train_and_deploy_model.sh
+++ ltr/concourse/train_and_deploy_model.sh
@@ -6,7 +6,7 @@ sudo apt-get install -y awscli git
 # docker setup from https://docs.docker.com/install/linux/docker-ce/ubuntu/
 sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu \$(lsb_release -cs) stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu `lsb_release -cs` stable"
 sudo apt-get update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 
