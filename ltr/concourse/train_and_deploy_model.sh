4e71385f333a47637b565b9e2d89ea306c74d44f
Sean Rankine <sean.rankine@me.com>
2022-01-07 16:42:44 +0000

    install python and pip

--- ltr/concourse/train_and_deploy_model.sh
+++ ltr/concourse/train_and_deploy_model.sh
@@ -8,7 +8,11 @@ sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent sof
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu `lsb_release -cs` stable"
 sudo apt-get update
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+sudo apt-get -y upgrade python3
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io python3-pip
+
+alias python=python3
+alias pip=pip3
 
 cd /tmp
 git clone --depth 1 --branch $GIT_BRANCH https://github.com/alphagov/search-api.git
