28f142b8d713cee9360e38df2c437737eba5380d
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2023-04-03 16:01:13 +0100

    Test disabling LTR

--- lib/search/relevance_helpers.rb
+++ lib/search/relevance_helpers.rb
@@ -1,5 +1,6 @@
 module Search::RelevanceHelpers
   def self.ltr_enabled?
-    ENV["ENABLE_LTR"].present? && ENV["ENABLE_LTR"] == "true"
+    #ENV["ENABLE_LTR"].present? && ENV["ENABLE_LTR"] == "true"
+    false
   end
 end
