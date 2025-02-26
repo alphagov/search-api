55fb41da22f116f13f36679d5b843216cc2d0d05
Ryan Brown <ryan.brown@digital.cabinet-office.gov.uk>
2025-02-26 13:25:43 +0000

    Use the bulk Sidekiq queue for bulk reindexing jobs
    We have a separate "bulk" sidekiq queue for running bulk reindexing loads so that publisher-driven indexing will not be blocked in production.
    
    However, the GOVUK index publishing event processor was not respecting the routing key provided in the RabbitMQ queue message, and therefore the default Sidekiq queue was being blocked by reindexing operations.
    
    This commit fixes the issue at the cost of an extra loop through the message batch to partition the messages into bulk and non-bulk messages.

--- lib/govuk_index/publishing_event_processor.rb
+++ lib/govuk_index/publishing_event_processor.rb
@@ -4,7 +4,13 @@ module GovukIndex
       messages = Array(messages) # treat a single message as an array with one value
 
       Services.statsd_client.increment("govuk_index.rabbit-mq-consumed")
-      PublishingEventJob.perform_async(messages.map { |msg| [msg.delivery_info[:routing_key], msg.payload] })
+
+      bulk_reindex_messages, default_messages = messages.partition do |msg|
+        msg.delivery_info[:routing_key].end_with?(".bulk.reindex")
+      end
+
+      PublishingEventJob.set(queue: "bulk").perform_async(bulk_reindex_messages.map { |msg| [msg.delivery_info[:routing_key], msg.payload] })
+      PublishingEventJob.perform_async(default_messages.map { |msg| [msg.delivery_info[:routing_key], msg.payload] })
       messages.each(&:ack)
     end
   end
