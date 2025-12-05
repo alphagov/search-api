895fb900a706359dce6d95f72dfb08ba82a8a7bf
Emma <emma.lowe@digital.cabinet-office.gov.uk>
2025-12-05 16:04:12 +0000

    Add method context to missing metadata tests

--- spec/integration/missing_metadata_spec.rb
+++ spec/integration/missing_metadata_spec.rb
@@ -1,64 +1,66 @@
 require "spec_helper"
 
 RSpec.describe "MissingMetadataTest" do
-  it "finds missing content_id" do
-    commit_document(
-      "government_test",
-      {
-        "link" => "/path/to_page",
-      },
-    )
+  describe "#retrieve_records_with_missing_value" do
+    it "finds missing content_id" do
+      commit_document(
+        "government_test",
+        {
+          "link" => "/path/to_page",
+        },
+      )
 
-    runner = MissingMetadata::Runner.new("content_id", search_config: SearchConfig.default_instance, logger: io)
-    results = runner.retrieve_records_with_missing_value
+      runner = MissingMetadata::Runner.new("content_id", search_config: SearchConfig.default_instance, logger: io)
+      results = runner.retrieve_records_with_missing_value
 
-    expect([{ _id: "/path/to_page", index: "government_test" }]).to eq results
-  end
+      expect([{ _id: "/path/to_page", index: "government_test" }]).to eq results
+    end
 
-  it "ignores already set content_id" do
-    commit_document(
-      "government_test",
-      {
-        "link" => "/path/to_page",
-        "content_id" => "8aea1742-9cc6-4dfb-a63b-12c3e66a601f",
-      },
-    )
+    it "ignores already set content_id" do
+      commit_document(
+        "government_test",
+        {
+          "link" => "/path/to_page",
+          "content_id" => "8aea1742-9cc6-4dfb-a63b-12c3e66a601f",
+        },
+      )
 
-    runner = MissingMetadata::Runner.new("content_id", search_config: SearchConfig.default_instance, logger: io)
-    results = runner.retrieve_records_with_missing_value
+      runner = MissingMetadata::Runner.new("content_id", search_config: SearchConfig.default_instance, logger: io)
+      results = runner.retrieve_records_with_missing_value
 
-    expect(results).to be_empty
-  end
+      expect(results).to be_empty
+    end
 
-  it "finds missing document_type" do
-    commit_document(
-      "government_test",
-      {
-        "link" => "/path/to_page",
-        "content_id" => "8aea1742-9cc6-4dfb-a63b-12c3e66a601f",
-      },
-    )
+    it "finds missing document_type" do
+      commit_document(
+        "government_test",
+        {
+          "link" => "/path/to_page",
+          "content_id" => "8aea1742-9cc6-4dfb-a63b-12c3e66a601f",
+        },
+      )
 
-    runner = MissingMetadata::Runner.new("content_store_document_type", search_config: SearchConfig.default_instance, logger: io)
-    results = runner.retrieve_records_with_missing_value
+      runner = MissingMetadata::Runner.new("content_store_document_type", search_config: SearchConfig.default_instance, logger: io)
+      results = runner.retrieve_records_with_missing_value
 
-    expect([{ _id: "/path/to_page", index: "government_test", content_id: "8aea1742-9cc6-4dfb-a63b-12c3e66a601f" }]).to eq results
-  end
+      expect([{ _id: "/path/to_page", index: "government_test", content_id: "8aea1742-9cc6-4dfb-a63b-12c3e66a601f" }]).to eq results
+    end
 
-  it "ignores already set document_type" do
-    commit_document(
-      "government_test",
-      {
-        "link" => "/path/to_page",
-        "content_id" => "8aea1742-9cc6-4dfb-a63b-12c3e66a601f",
-        "content_store_document_type" => "guide",
-      },
-    )
+    it "ignores already set document_type" do
+      commit_document(
+        "government_test",
+        {
+          "link" => "/path/to_page",
+          "content_id" => "8aea1742-9cc6-4dfb-a63b-12c3e66a601f",
+          "content_store_document_type" => "guide",
+        },
+      )
 
-    runner = MissingMetadata::Runner.new("content_store_document_type", search_config: SearchConfig.default_instance, logger: io)
-    results = runner.retrieve_records_with_missing_value
+      runner = MissingMetadata::Runner.new("content_store_document_type", search_config: SearchConfig.default_instance, logger: io)
+      results = runner.retrieve_records_with_missing_value
 
-    expect(results).to be_empty
+      expect(results).to be_empty
+    end
   end
 
   def io
