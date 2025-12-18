5ed2e5547fef3b662a2ae82ca6590f65334eb755
koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2025-12-18 10:05:13 +0000

    Limit GA4 path length before indexing into Elasticsearch
    We have been seeing a large number of errors from Sidekiq::GovukIndex::PageTrafficJob.
    The root cause is malformed data retrieved from GA4, which the job attempts to index into Elasticsearch.
    
    Elasticsearch rejects these documents because the base_path field exceeds its 512-byte limit.
    
    GOV.UK paths should never be this long, so values exceeding 512 bytes can be safely treated as invalid.
    This change filters out those paths before indexing.

--- lib/analytics/ga4_import/page_view_consolidator.rb
+++ lib/analytics/ga4_import/page_view_consolidator.rb
@@ -1,6 +1,8 @@
 module Analytics
   module Ga4Import
     class PageViewConsolidator
+      MAX_PATH_LENGTH = 512
+
       def initialize(ga_data)
         @ga_data = ga_data
       end
@@ -8,6 +10,7 @@ module Analytics
       def consolidated_page_views
         ga_data
           .reject(&:excluded?)
+          .reject { |page_data| page_data.path.bytesize >= MAX_PATH_LENGTH }
           .group_by(&:normalised_path)
           .transform_values { |grouped_pages| grouped_pages.sum(&:page_views) }
           .sort_by { |_, views| -views }
