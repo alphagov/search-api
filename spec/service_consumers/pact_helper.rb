370eb0602045d4264ebd11e13f4b8c8d94b9532f
Lucy Stringer <lucy.stringer@digital.cabinet-office.gov.uk>
2026-01-09 11:21:33 +0000

    Add initial provider state Further provider states will follow in a subsequent PR. Initially just adding a test for a basic search, to check that thet pact tests are configured correctly. Refactored IntegrationSpecHelper to allow for some methods to be reused by pact tests while others are only used by integration tests.

---
+++ spec/service_consumers/pact_helper.rb
@@ -0,0 +1,63 @@
+ENV["PACT_DO_NOT_TRACK"] = "true"
+ENV["RACK_ENV"] = "test"
+
+require "pact/provider/rspec"
+require "webmock/rspec"
+require "gds_api"
+
+require_relative "../../spec/support/index_helpers"
+require_relative "../../spec/support/integration_test_helper"
+
+Pact.configure do |config|
+  config.reports_dir = "spec/reports/pacts"
+  config.include WebMock::API
+  config.include WebMock::Matchers
+  config.include IntegrationTestHelper
+end
+
+def pact_broker_base_url
+  "https://govuk-pact-broker-6991351eca05.herokuapp.com"
+end
+
+Pact.service_provider "Search API" do
+  include ERB::Util
+
+  honours_pact_with "GDS API Adapters" do
+    if ENV["PACT_URI"]
+      pact_uri(ENV["PACT_URI"])
+    else
+      path = "pacts/provider/#{url_encode(name)}/consumer/#{url_encode(consumer_name)}"
+      version_modifier = "versions/#{url_encode(ENV.fetch('PACT_CONSUMER_VERSION', 'branch-main'))}"
+      pact_uri("#{pact_broker_base_url}/#{path}/#{version_modifier}")
+    end
+  end
+end
+
+Pact.provider_states_for "GDS API Adapters" do
+  set_up do
+    WebMock.enable!
+    WebMock.reset!
+    IntegrationTestHelper.allow_elasticsearch_connection_to_test(pact_broker_base_url)
+    IndexHelpers.setup_test_indexes
+  end
+
+  tear_down do
+    IndexHelpers.clean_all
+    WebMock.disable!
+  end
+
+  provider_state "there are search results for universal credit" do
+    set_up do
+      document_params = {
+        "title" => "Universal credit",
+        "link" => "/universal-credit",
+      }
+      second_document_params = {
+        "title" => "Universal credit too",
+        "link" => "/universal-credit-too",
+      }
+      commit_document("government_test", document_params)
+      commit_document("government_test", second_document_params)
+    end
+  end
+end
