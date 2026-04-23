a93fc3cd8b53f8aa8111a1d9b998245142e12071
koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2026-04-23 10:47:16 +0100

    deprecate the DELETE /:index/documents.* endpoint
    This work is part of the work to decommission the ‘government’ index.
    
    All updates to the Elasticsearch index are now done through RabbitMQ so
    this endpoint can be removed.
    
    Log any attempt to access this endpoint

--- spec/integration/app/deprecated_endpoints_spec.rb
+++ spec/integration/app/deprecated_endpoints_spec.rb
@@ -1,12 +1,19 @@
 require "spec_helper"
 
-RSpec.describe "ContentEndpointsTest" do
+RSpec.describe "Deprecate content endpoints" do
   before do
     allow(GovukError).to receive(:notify)
   end
-  it "returns an error" do
-    post "/govuk_test/documents"
-    expect(last_response.status).to eq 403
-    expect(GovukError).to have_received(:notify)
+
+  shared_examples "forbidden request" do |http_method, path|
+    it "returns 403 and notifies GovukError for #{http_method.upcase} #{path}" do
+      send(http_method, path)
+
+      expect(last_response.status).to eq(403)
+      expect(GovukError).to have_received(:notify)
+    end
   end
+
+  include_examples "forbidden request", :post, "/govuk_test/documents"
+  include_examples "forbidden request", :delete, "/govuk_test/documents/link"
 end
