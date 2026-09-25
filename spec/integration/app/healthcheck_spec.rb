dd7315917e3d36ca10fa649244f42b54d1d6ab50
Jos Koetsier <Jos.Koetsier@digital.cabinet-office.gov.uk>
2026-09-25 14:27:58 +0100

    Update es_source to os_source

--- spec/integration/app/healthcheck_spec.rb
+++ spec/integration/app/healthcheck_spec.rb
@@ -39,8 +39,8 @@ RSpec.describe "HealthcheckTest" do
   describe "#opensearch_connectivity check" do
     context "when opensearch CANNOT be connected to" do
       it "returns a critical status" do
-        es_source = ENV["OPENSEARCH_URI"] || "http://localhost:9200"
-        stub_request(:get, %r{#{es_source}/_cluster/health}).to_raise(Errno::ECONNREFUSED)
+        os_source = ENV["OPENSEARCH_URI"] || "http://localhost:9200"
+        stub_request(:get, %r{#{os_source}/_cluster/health}).to_raise(Errno::ECONNREFUSED)
 
         get "/healthcheck/ready"
 
